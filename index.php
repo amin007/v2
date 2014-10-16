ae5d08b4f6208cb0ef530c0643ad149965efaf46
Amin PO Muar <helmi007@yahoo.com>
2014-10-16 13:33:04 +0800

    guna # untuk komen tentang koding guna // untuk sembunyikan kod sedia ada

--- index.php
+++ index.php
@@ -8,21 +8,21 @@
  * 4. masukkan semua fail class dari folder PUSTAKA
  * 5. istihar class Mulakan
  */
-// 1. laporan tahap kesilapan kod PHP 
-//error_reporting(E_ALL);
+# 1. laporan tahap kesilapan kod PHP 
+error_reporting(E_ALL);
 
-// 2. isytiharkan zon masa => Asia/Kuala Lumpur
+# 2. isytiharkan zon masa => Asia/Kuala Lumpur
 date_default_timezone_set('Asia/Kuala_Lumpur');
 
-// 3. setkan tatarajah sistem
+# 3. setkan tatarajah sistem
 require 'tatarajah.php';
 
-// 4. masukkan semua fail class dari folder PUSTAKA
-//    Also spl_autoload_register (Take a look at it if you like)
+# 4. masukkan semua fail class dari folder PUSTAKA
+#    also spl_autoload_register (Take a look at it if you like)
 function __autoload($class) 
 {
 	require PUSTAKA . $class . '.php';
 }
 
-// 5. istihar class Mulakan
+# 5. istihar class Mulakan
 $aplikasi = new Mulakan();
