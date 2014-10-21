90fa2f4d50d682a0ef21254463cb1214bd62f854
Amin PO Muar <helmi007@yahoo.com>
2014-10-21 16:55:44 +0800

    matikan iklan nuffang kalau di localhost

---
+++ aplikasi/papar/index/index.php
@@ -0,0 +1,63 @@
+<div class="container">
+    <div class="row">
+    <div class="span12">
+    <h1 style="text-align:center;background-color:white;">Tentang Amin007<img 
+	src="<?php echo GAMBAR ?>bg/logo/amin007_hijau.jpg" alt="logo" 
+	width="93" height="101"></h1>
+	<br>
+	<div class="row">
+		<div class="span3"><img 
+		src="<?php echo GAMBAR ?>bg/jpg/amin.jpg" width="290" height="422" alt="gambar saya">
+		</div>
+		<div class="span7" style="background-color:white;text-align:justify;">
+			<h2>&nbsp;Assalamualaikum &amp; Selamat Datang Ke <br> Dunia Imaginasi Saya</h2>
+			<p>Nama saya Amin, berasal dari muar, johor. 
+			Laman web ini menceritakan tentang <b>Dunia Imaginasi Saya </b>dimana
+			saya akan paparkan kepada anda semua biodata saya, gambar-gambar saya, 
+			fail-fail atau perisian yang biasa saya guna, projek-projek yang sedang atau 
+			telah saya jalankan dan juga subdomain dalam laman web saya.
+			<br><br>
+			Anda semua mesti tertanya-tanya kepada saya menggunakan tajuk <b>Dunia Imaginasi Saya. </b> 
+			sebab dari kecil lagi saya suka berangan-angan yang mana banyak dipengaruhi oleh 
+			buku-buku bercorak sains &amp; penyiasatan di perpustakaan awam johor cawangan muar 
+			&amp; filem-filem di tv terutama James Bond. Tapi imaginasi saya lebih dalam sains &amp; 
+			teknologi. saya banyak menghasilkan idea-idea yang dianggap gila bagi sesetengah 
+			pihak. namun saya tak kisah sebab dalam sejarah dunia membuktikan banyak inovasi 
+			dihasilkan dari idea kreatif yang dianggap gila.
+			<br><br>
+			Tetapi sekarang saya banyak ke kedai buku sebab disitu banyak buku-buku 
+			terbaru dalam pasaran. Banyak ilham yang saya dapat apabila ke kedai buku. 
+			Antara buku yang saya minat bercorak motivasi, agama, falsafah dan komputer. 
+			Tapi minat saya cenderung kepada komputer sebab ia memudahkan apa-apa ilham dari 
+			kepala dipaparkan terus ke monitor &amp; boleh dicetak.
+			<br><br>
+			<a target="_blank" style="font-size: 20pt; text-decoration: overline underline; background-color: #000000" 
+			href="http://www.jangkaan.name.my/blog/">Jangkaan Amin007</a> adalah blog saya yang lain pula.
+			Saya harap anda berilah komen yang membina agar saya akan paparkan pada umum.
+			setakat ini topik blog meliputi diri sendiri, tutorial php, ekonomi, cinta, agama, politik dan sebagainya. 
+			<br><br>
+			Ialah, kalau dah baca buku banyak-banyak, saya rasa nak menulis pulak :D
+			Jadi saya jadikan blog sebagai pemulaan penulisan saya. mungkin satu hari nanti saya
+			akan pecahkan topik-topik berkaitan menjadi satu blog yang fokus pada topik tertentu.
+			lagipun website saya ini amat popular, cuma blog saya aje kurang popular.
+			ini adalah salah satu usaha jadikan dia popular.
+			<br><br>
+			Lagipun saya sekarang tengah nak design satu sistem yang mengabungkan blog dan forum.
+			cuma tak terfikir lagi nak letak apa nama hehehe. Harap anda seronok melayari <b>Dunia Imaginasi Saya.</b> 
+			emaillah kepada saya di al.amin[at]yahoo.com jika ada cadangan yang baik-baik. Saya 
+			akan cuba kemaskini kandungan ni dari semasa ke semasa.
+			<br><br>
+			Sekian Terima Kasih. Wasalam</p>
+		</div>
+		<div class="span1"><?php
+/*
+		<!-- nuffnang -->
+		<script type="text/javascript">nuffnang_bid = "2bb5639c70db0006532074b0c35f4b93";</script>
+		<script type="text/javascript" src="http://synad2.nuffnang.com.my/lmn.js"></script>
+		<!-- nuffnang-->
+*/?>
+		</div>
+	</div>
+    </div>
+    </div>	
+</div>
