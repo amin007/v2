a028a8d6aa34a84bc65e3bf0c51ebec4efe4d18c
Amin PO Muar <helmi007@yahoo.com>
2014-10-21 17:13:02 +0800

    ubah iklan nuffang kalau di localhost, tidak perlu paparkan iklan.

---
+++ aplikasi/papar/diatas.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title><?php
+$namaServer = $_SERVER["SERVER_NAME"];
+$tajuk[0] = (($namaServer=='localhost') || ($namaServer=='sidapmuar') )? 
+"Ini kat komputer kaulah":" Di Amin007 Homepage";
+
+$tajuk[1] = $this->Tajuk_Muka_Surat; // papar title
+
+$dpt_url = dpt_url();
+$tajuk[2] = (empty($dpt_url[2])) ? null 
+	: '/' . $dpt_url[2];
+	
+echo $tajuk[0] . '[' . $tajuk[1] . $tajuk[2] . ']' ;
+ ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Nama saya Amin, berasal dari muar, johor. 
+Laman web ini menceritakan tentang Dunia Imaginasi Saya dimana
+saya akan paparkan kepada anda semua biodata saya, gambar-gambar saya, 
+fail-fail atau perisian yang biasa saya guna, projek-projek yang sedang atau 
+telah saya jalankan dan juga subdomain dalam laman web saya">
+<meta name="keywords" content="idea kreatif inovatif projek link Dunia Imaginasi Saya">
+<meta name="author" content="Amin007 Ledang">
+<?php
+require 'diatas-bootstrap.php';
+?>
+<link rel="stylesheet" href="<?php echo JS ?>public/css/gambar_head.css" />
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td 
+{ 
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+</style>
+<script type='text/javascript'>
+<!-- //Mula Papar Jam
+var dayarray=new Array("Ahad","Isnin","Selasa","Rabu","Khamis","Jumaat","Sabtu")
+var bulan=new Array("Januari","Februari","Mac","April","Mei","Jun",
+"Julai","Ogos","September","Oktober","November","Disember")
+function getthedate()
+{
+	var mydate=new Date()
+	var year=mydate.getYear()
+	if (year < 1000) year+=1900
+	var day=mydate.getDay()
+	var month=mydate.getMonth()
+	var daym=mydate.getDate()
+	if (daym<10) daym="0"+daym
+	var hours=mydate.getHours()
+	var minutes=mydate.getMinutes()
+	var seconds=mydate.getSeconds()
+	var dn="AM"
+	if (hours>=12)dn="PM"
+	if (hours>12){hours=hours-12}
+	{
+	 d = new Date(); Time24H = new Date();
+	 Time24H.setTime(d.getTime() + (d.getTimezoneOffset()*60000) + 3600000);
+	 InternetTime = Math.round((Time24H.getHours()*60+Time24H.getMinutes()) / 1.44);
+	 if (InternetTime < 10) InternetTime = '00'+InternetTime;
+	 else if (InternetTime < 100) InternetTime = '0'+InternetTime;
+	}
+	if (hours==0)hours=12
+	if (minutes<=9)minutes="0"+minutes
+	if (seconds<=9)seconds="0"+seconds
+	//change font size here
+	var cdate=" ["+dayarray[day]+" "+daym+" "+bulan[month]+" "+year+
+	". Pukul "+hours+":"+minutes+":"+seconds+" "+dn+ " ]"
+
+	if (document.all) 
+		{document.all.masa.innerHTML=cdate;
+		document.all.clock.innerHTML=cdate;}
+	else if (document.getElementById)
+		{document.getElementById("masa").innerHTML=cdate;
+		document.getElementById("clock").innerHTML=cdate;}
+	else document.write(cdate)
+}
+if (!document.all&&!document.getElementById) getthedate()
+
+function goforit()
+{
+if (document.all||document.getElementById)
+setInterval("getthedate()",1000)
+}
+window.onload=goforit
+//function onload(){goforit();populate();} 
+//  Tutup Papar Jam -->
+</script><?php
+$namaServer = $_SERVER["SERVER_NAME"];
+if (($namaServer=='localhost') || ($namaServer=='sidapmuar') ):
+	echo null;
+else:?>
+<!-- nuffnang -->
+<script type="text/javascript">
+nuffnang_bid = "2bb5639c70db0006532074b0c35f4b93";
+</script>
+<script type="text/javascript" src="http://synad2.nuffnang.com.my/k.js"></script>
+<!-- nuffnang-->
+<?php
+endif;
+
+if (isset($this->css)) 
+{
+	foreach ($this->css as $css)
+	{
+		//echo "\n" . '<script type="text/javascript" src="' . JS . $js .'"></script>';
+		echo "\n" . '<link rel="stylesheet" type="text/css" href="' . JS . $css .'">';
+	}
+}
+
+/*
+<img src="http://bssu.amin007.org/bg/bg/<?php print_r($this->gambar); ?>" />
+<!-- background="<?php echo GAMBAR . $this->gambar ?>" -->
+<img src="<?php echo GAMBAR . $this->gambar ?>" alt="background image" id="bg">
+*/
+
+$lokasi = 'http://bssu.amin007.org/bg/bg/';
+
+?>
+
+</head>  
+<body<?php 
+echo '  style="background-color:black;"'
+//echo ' background="' . $lokasi . $this->gambar . '"' ?>>
---
+++ aplikasi/papar/warnawarni.php
@@ -0,0 +1,352 @@
+<div class="container" style="background-color:black;color:white;">
+    <div class="row">
+    <div class="span10">
+	<h1 style="text-align:center;color:white;">
+	<?php echo $this->Tajuk_Muka_Surat; ?></h1>
+<!-- senarai warna -- mula -->
+<table width="80%" border=1 align=center>
+  <caption>Beribu-ribu Warna Warni</caption>
+  <tbody>
+    <tr>
+    <td width="20%">aliceblue=#f0f8ff</td>
+    <td width="5%" bgColor=#f0f8ff>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+    </td>
+    <td width="23%">antiquewhite=#faebd7</td>
+    <td width="8%" bgColor=#faebd7>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+    </td>
+    <td width="27%">aqua*=#00ffff</td>
+    <td width="7%" bgColor=#00ffff>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+</td></tr>
+  <tr>
+    <td>aquamarine=#7fffd4</td>
+    <td bgColor=#7fffd4>&nbsp;</td>
+    <td width="23%">azure=#f0ffff</td>
+    <td width="8%" bgColor=#f0ffff>&nbsp;</td>
+    <td width="27%">beige=#f5f5dc</td>
+    <td width="7%" bgColor=#f5f5dc>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">bisque=#ffe4c4</td>
+    <td width="8%" bgColor=#ffe4c4>&nbsp;</td>
+    <td width="23%">black=#000000</td>
+    <td width="8%" bgColor=#000000>&nbsp;</td>
+    <td width="27%">blanchedalmond=#ffebcd</td>
+    <td width="7%" bgColor=#ffebcd>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">blue*=#0000ff</td>
+    <td width="8%" bgColor=#0000ff>&nbsp;</td>
+    <td width="23%">blueviolet=#8a2be2</td>
+    <td width="8%" bgColor=#8a2be2>&nbsp;</td>
+    <td width="27%">brown=#a52a2a</td>
+    <td width="7%" bgColor=#a52a2a>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">burlywood=#deb887</td>
+    <td width="8%" bgColor=#deb887>&nbsp;</td>
+    <td width="23%">cadetblue=#5f9ea0</td>
+    <td width="8%" bgColor=#5f9ea0>&nbsp;</td>
+    <td width="27%">charteuse=#7fff00</td>
+    <td width="7%" bgColor=#7fff00>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">chocolate=#d2691e</td>
+    <td width="8%" bgColor=#d2691e>&nbsp;</td>
+    <td width="23%">coral=#ff7f50</td>
+    <td width="8%" bgColor=#ff7f50>&nbsp;</td>
+    <td width="27%">cornflowerblue=#6495ed</td>
+    <td width="7%" bgColor=#6495ed>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">cornsilk=#fff8dc</td>
+    <td width="8%" bgColor=#fff8dc>&nbsp;</td>
+    <td width="23%">crimson=#dc143c</td>
+    <td width="8%" bgColor=#dc143c>&nbsp;</td>
+    <td width="27%">cyan=#00ffff</td>
+    <td width="7%" bgColor=#00ffff>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">darkblue=#00008b</td>
+    <td width="8%" bgColor=#00008b>&nbsp;</td>
+    <td width="23%">darkcyan=#008b8b</td>
+    <td width="8%" bgColor=#008b8b>&nbsp;</td>
+    <td width="27%">darkgoldrod=#b8860b</td>
+    <td width="7%" bgColor=#b8860b>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lightsalmon=#ffa07a</td>
+    <td width="8%" bgColor=#ffa07a>&nbsp;</td>
+    <td width="23%">lightseagreen=#20b2aa</td>
+    <td width="8%" bgColor=#20b2aa>&nbsp;</td>
+    <td width="27%">lightskyblue=#87cefa</td>
+    <td width="7%" bgColor=#87cefa>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lightslatgray=#778899</td>
+    <td width="8%" bgColor=#778899>&nbsp;</td>
+    <td width="23%">lightsteelblue=#b0c4de</td>
+    <td width="8%" bgColor=#b0c4de>&nbsp;</td>
+    <td width="27%">lightyellow=#ffffe0</td>
+    <td width="7%" bgColor=#ffffe0>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lime*=#00ff00</td>
+    <td width="8%" bgColor=#00ff00>&nbsp;</td>
+    <td width="23%">limegreen=#32cd32</td>
+    <td width="8%" bgColor=#32cd32>&nbsp;</td>
+    <td width="27%">linen=#faf0e6</td>
+    <td width="7%" bgColor=#faf0e6>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">magenta=#ff00ff</td>
+    <td width="8%" bgColor=#ff00ff>&nbsp;</td>
+    <td width="23%">maroon*=#800000</td>
+    <td width="8%" bgColor=#800000>&nbsp;</td>
+    <td width="27%">mediumaquamarine=#66cdaa</td>
+    <td width="7%" bgColor=#66cdaa>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">mediumblue=#0000cd</td>
+    <td width="8%" bgColor=#0000cd>&nbsp;</td>
+    <td width="23%">mediumorhid=#ba55d3</td>
+    <td width="8%" bgColor=#ba55d3>&nbsp;</td>
+    <td width="27%">mediumpurple=#9370db</td>
+    <td width="7%" bgColor=#9370db>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">mediumseagreen=#3cb371</td>
+    <td width="8%" bgColor=#3cb371>&nbsp;</td>
+    <td width="23%">mediumslateblue=#7b68ee</td>
+    <td width="8%" bgColor=#7b68ee>&nbsp;</td>
+    <td width="27%">mediumspringgreen=#00fa9a</td>
+    <td width="7%" bgColor=#00fa9a>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">mediumturquoise=#48d1cc</td>
+    <td width="8%" bgColor=#48d0cc>&nbsp;</td>
+    <td width="23%">mediumvioletred=#c71585</td>
+    <td width="8%" bgColor=#c71585>&nbsp;</td>
+    <td width="27%">midnightblue=#191970</td>
+    <td width="7%" bgColor=#191970>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">mintcream=#f5fffa</td>
+    <td width="8%" bgColor=#f5fffa>&nbsp;</td>
+    <td width="23%">mistyrose=#ffe4e1</td>
+    <td width="8%" bgColor=#ffe4e0>&nbsp;</td>
+    <td width="27%">moccasin=#ffe4b5</td>
+    <td width="7%" bgColor=#ffe4b5>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">darkgray=#a9a9a9</td>
+    <td width="8%" bgColor=#a9a9a9>&nbsp;</td>
+    <td width="23%">darkgreen=#006400</td>
+    <td width="8%" bgColor=#006400>&nbsp;</td>
+    <td width="27%">darkkhaki=#bdb76b</td>
+    <td width="7%" bgColor=#bdb76b>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">darkolivegreen=#55b62f</td>
+    <td width="8%" bgColor=#55b62f>&nbsp;</td>
+    <td width="23%">darkorange=#ff8c00</td>
+    <td width="8%" bgColor=#ff8c00>&nbsp;</td>
+    <td width="27%">darkorhid=#9932cc</td>
+    <td width="7%" bgColor=#9932cc>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">darkred=#8b0000</td>
+    <td width="8%" bgColor=#8b0000>&nbsp;</td>
+    <td width="23%">darksalmon=#e9967a</td>
+    <td width="8%" bgColor=#e9967a>&nbsp;</td>
+    <td width="27%">darkseagreen=#8fbc8f</td>
+    <td width="7%" bgColor=#8fbc8f>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">darkslateblue=#483d8b</td>
+    <td width="8%" bgColor=#483d8b>&nbsp;</td>
+    <td width="23%">darkslategray=#2f4f4f</td>
+    <td width="8%" bgColor=#2f4f4f>&nbsp;</td>
+    <td width="27%">darkturquoise=#00ced1</td>
+    <td width="7%" bgColor=#00ced1>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">darkviolet=#9400d3</td>
+    <td width="8%" bgColor=#9400d3>&nbsp;</td>
+    <td width="23%">deeppink=#ff1493</td>
+    <td width="8%" bgColor=#ff1493>&nbsp;</td>
+    <td width="27%">deepskyblue=#00bfbf</td>
+    <td width="7%" bgColor=#00bfbf>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">dimgray=#696969</td>
+    <td width="8%" bgColor=#696969>&nbsp;</td>
+    <td width="23%">dodgerblue=#1e90ff</td>
+    <td width="8%" bgColor=#1e99ff>&nbsp;</td>
+    <td width="27%">firebrick=#b22222</td>
+    <td width="7%" bgColor=#b22222>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">floralwhite=#fffaf0</td>
+    <td width="8%" bgColor=#fffaf0>&nbsp;</td>
+    <td width="23%">forestgreen=#228b22</td>
+    <td width="8%" bgColor=#228b22>&nbsp;</td>
+    <td width="27%">fichsia*=#ff00ff</td>
+    <td width="7%" bgColor=#ff00ff>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">gainsboro=#dcdcdc</td>
+    <td width="8%" bgColor=#dcdcdc>&nbsp;</td>
+    <td width="23%">ghostwhite=#f8f8ff</td>
+    <td width="8%" bgColor=#f8f8ff>&nbsp;</td>
+    <td width="27%">gold=#ffd700</td>
+    <td width="7%" bgColor=#ffd700>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">goldenrod=#daa520</td>
+    <td width="8%" bgColor=#daa502>&nbsp;</td>
+    <td width="23%">gray*=#808080</td>
+    <td width="8%" bgColor=#808080>&nbsp;</td>
+    <td width="27%">green*=#008000</td>
+    <td width="7%" bgColor=#008000>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">greenyellow=#adff2f</td>
+    <td width="8%" bgColor=#adff2f>&nbsp;</td>
+    <td width="23%">honeydew=#f0fff0</td>
+    <td width="8%" bgColor=#f0fff0>&nbsp;</td>
+    <td width="27%">hotpink=#ff69b4</td>
+    <td width="7%" bgColor=#ff69b4>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">indianred=#cd5c5c</td>
+    <td width="8%" bgColor=#cd5c5c>&nbsp;</td>
+    <td width="23%">indigo=#4b0082</td>
+    <td width="8%" bgColor=#4b0082>&nbsp;</td>
+    <td width="27%">ivory=#fffff0</td>
+    <td width="7%" bgColor=#fffff0>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">khaki=#f0e68c</td>
+    <td width="8%" bgColor=#f0e68c>&nbsp;</td>
+    <td width="23%">lavender=#e6e6fa</td>
+    <td width="8%" bgColor=#e6e6fa>&nbsp;</td>
+    <td width="27%">lavenderblush=#fff0f5</td>
+    <td width="7%" bgColor=#fff0f5>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lawngreen=#7cfc00</td>
+    <td width="8%" bgColor=#7cfc00>&nbsp;</td>
+    <td width="23%">lemonchiffon=#fffacd</td>
+    <td width="8%" bgColor=#fffacd>&nbsp;</td>
+    <td width="27%">lightblue=#add8e6</td>
+    <td width="7%" bgColor=#add8e6>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lightcoral=#f08080</td>
+    <td width="8%" bgColor=#f08080></td>
+    <td width="23%">navajowhite=#ffdead</td>
+    <td width="8%" bgColor=#ffdead>&nbsp;</td>
+    <td width="27%">navy*=#000080&nbsp;</td>
+    <td width="7%" bgColor=#000080>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">oldlace=#fdf5e6&nbsp;</td>
+    <td width="8%" bgColor=#fdf5e6>&nbsp;</td>
+    <td width="23%">olive*=#808000&nbsp;</td>
+    <td width="8%" bgColor=#808000>&nbsp;</td>
+    <td width="27%">olivedrab=#6b8e23&nbsp;</td>
+    <td width="7%" bgColor=#6b8e23>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">orange=#ffa500&nbsp;</td>
+    <td width="8%" bgColor=#ffa500>&nbsp;</td>
+    <td width="23%">orangered=#ff4500&nbsp;</td>
+    <td width="8%" bgColor=#ff4500>&nbsp;</td>
+    <td width="27%">orchid=#da70d6&nbsp;</td>
+    <td width="7%" bgColor=#da70d6>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">palegoldenrod=#eee8aa&nbsp;</td>
+    <td width="8%" bgColor=#eee8aa>&nbsp;</td>
+    <td width="23%">palegreen=#98fb98&nbsp;</td>
+    <td width="8%" bgColor=#98fb98>&nbsp;</td>
+    <td width="27%">paleturquise =#afeeee</td>
+    <td width="7%" bgColor=#afeeee>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">palevioletred=#db7093</td>
+    <td width="8%" bgColor=#db7093>&nbsp;</td>
+    <td width="23%">papayawhip=#ffefd5&nbsp;</td>
+    <td width="8%" bgColor=#ffefd5>&nbsp;</td>
+    <td width="27%">peachpuff=#ffdab9&nbsp;</td>
+    <td width="7%" bgColor=#ffdab9>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">peru=#cd853f&nbsp;</td>
+    <td width="8%" bgColor=#cd853f>&nbsp;</td>
+    <td width="23%">pink=#ffc0cb&nbsp;</td>
+    <td width="8%" bgColor=#ffc0cb>&nbsp;</td>
+    <td width="27%">plum=#dda0dd&nbsp;</td>
+    <td width="7%" bgColor=#dda0dd>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">powderblue=#b0e0e6&nbsp;</td>
+    <td width="8%" bgColor=#b0e0e6>&nbsp;</td>
+    <td width="23%">purple*=#800080&nbsp;</td>
+    <td width="8%" bgColor=#800080>&nbsp;</td>
+    <td width="27%">red*=#ff0000&nbsp;</td>
+    <td width="7%" bgColor=#ff0000>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">rosybrown=#bc8f8f&nbsp;</td>
+    <td width="8%" bgColor=#bc8f8f>&nbsp;</td>
+    <td width="23%">royalblue =#4169e1</td>
+    <td width="8%" bgColor=#4169e1>&nbsp;</td>
+    <td width="27%">saddlebrown=#8b4513</td>
+    <td width="7%" bgColor=#8b4513>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">salmon=#fa8072&nbsp;</td>
+    <td width="8%" bgColor=#fa8072>&nbsp;</td>
+    <td width="23%">sandybrown=#f4a460&nbsp;</td>
+    <td width="8%" bgColor=#f4a46>&nbsp;</td>
+    <td width="27%">seagreen=#2e8b57&nbsp;</td>
+    <td width="7%" bgColor=#2e8b57>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">seashell=#fff5ee&nbsp;</td>
+    <td width="8%" bgColor=#fff5ee>&nbsp;</td>
+    <td width="23%">sienna=#a0522d</td>
+    <td width="8%" bgColor=#a0522d>&nbsp;</td>
+    <td width="27%">silver*=#c0c0c0&nbsp;</td>
+    <td width="7%" bgColor=#c0c0c0>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">skyblue=#87ceeb&nbsp;</td>
+    <td width="8%" bgColor=#87ceeb>&nbsp;</td>
+    <td width="23%">slateblue=#6a5acd&nbsp;</td>
+    <td width="8%" bgColor=#6a5acd>&nbsp;</td>
+    <td width="27%">slategray=#708090&nbsp;</td>
+    <td width="7%" bgcolor=#708090>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">snow=#fffafa&nbsp;</td>
+    <td width="8%" bgColor=#fffafa>&nbsp;</td>
+    <td width="23%">springgreen=#00ff7f&nbsp;</td>
+    <td width="8%" bgColor=#00ff7f>&nbsp;</td>
+    <td width="27%">stellblue#4682b4&nbsp;</td>
+    <td width="7%" bgColor=#4682b4>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">tan=#d2b48c&nbsp;</td>
+    <td width="8%" bgColor=#d2b48c>&nbsp;</td>
+    <td width="23%">teal*=#008080&nbsp;</td>
+    <td width="8%" bgColor=#008080>&nbsp;</td>
+    <td width="27%">thistle=#d8bfd8&nbsp;</td>
+    <td width="7%" bgColor=#d8bfd8>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">tomato=#ff6347&nbsp;</td>
+    <td width="8%" bgColor=#ff6347>&nbsp;</td>
+    <td width="23%">turquise=#40e0do&nbsp;</td>
+    <td width="8%" bgColor=#40e0ed>&nbsp;</td>
+    <td width="27%">violet=#ee82ee</td>
+    <td width="7%" bgColor=#ee82ee>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lightcyan=#e0ffff</td>
+    <td width="8%" bgColor=#e0ffff>&nbsp;</td>
+    <td width="23%">lightgoldenrodyellow=#fafad2</td>
+    <td width="8%" bgColor=#fafad2>&nbsp;</td>
+    <td width="27%">lightgreen=#90ee90</td>
+    <td width="7%" bgColor=#90ee90>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">lightgrey=#d3d3d3</td>
+    <td width="8%" bgColor=#d3d3d3>&nbsp;</td>
+    <td width="23%">lightpink=#ffb6c1</td>
+    <td width="8%" bgColor=#ffb6c1>&nbsp;</td>
+    <td width="27%">wheat=#f5deb3</td>
+    <td width="7%" bgColor=#f5deb3>&nbsp;</td></tr>
+  <tr>
+    <td width="25%">whitemoke=#f5f5f5</td>
+    <td width="8%" bgColor=#f5f5f5>&nbsp;</td>
+    <td width="23%">yellow=#ffff00</td>
+    <td width="8%" bgColor=#ffff00>&nbsp;</td>
+    <td width="27%">yellowgreen#=9acd32</td>
+    <td width="7%" bgColor=#9acd32>&nbsp;</td>
+  </tr></tbody></table> 
+<!-- senarai warna -- tutup -->
+	</div>
+		<div class="span1"><?php 
+$namaServer = $_SERVER['SERVER_NAME'];
+if (($namaServer=='localhost') || ($namaServer=='sidapmuar') ):
+	echo null;
+else:?>
+		<!-- nuffnang -->
+		<script type="text/javascript">nuffnang_bid = "2bb5639c70db0006532074b0c35f4b93";</script>
+		<script type="text/javascript" src="http://synad2.nuffnang.com.my/lmn.js"></script>
+		<!-- nuffnang-->
+<?php
+endif;
+?>
+		</div>
+	</div>
+</div>
