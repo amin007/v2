4d6712f1f4f8b0114971902290a6b585ef9ee2ef
Amin Ledang@amin-hp <helmi007@yahoo.com>
2018-06-19 00:42:23 +0800

    buang fungsi baca()

--- Aplikasi/Kelas/Kitab/Papar.php
+++ Aplikasi/Kelas/Kitab/Papar.php
@@ -8,58 +8,6 @@ class Papar
 		//echo '<br>1. Anda berada di class Papar<br>';
 	}
 #==========================================================================================
-	public function baca($nama, $template, $noInclude = false)
-	{
-		//echo '<br>1.Anda berada di class Papar::' . $nama . '()<br>';
-
-		$namafail = explode('/', $nama);
-        $failPapar = GetMatchingFiles(
-			GetContents(PAPAR . '/' . $namafail[0]),
-			$namafail[1] . '.php');
-		$paparFail = $failPapar[0];
-		/*echo '<hr size=2>PAPAR=' . PAPAR . '<br>';
-        echo '$namafail=<pre>'; print_r($namafail) . '</pre><br>';
-        echo '$failPapar=<pre>'; print_r($failPapar) . '</pre><br>';
-		echo '$paparFail->' . $paparFail . '<br>';
-		//*/
-
-		$cariNama = array ('index/index', 'index/login',
-		'index/login_automatik','index/daftar','index/muar');
-		$cariJQM = array('mobile/mobile','mobile/iconjqm');
-
-		if ($paparFail == false) 
-		{
-			\Aplikasi\Kitab\Peta::failPaparTidakWujud();
-			//echo 'failPaparTidakWujud()';
-		}
-		//elseif (in_array($nama,$cariJQM)) 
-		elseif ( $namafail[0]=='mobile')
-		{
-			require PAPAR . '/diatas-jqm.php';
-			require $paparFail;
-			require PAPAR . '/dibawah-jqm.php';
-		}
-		elseif ($noInclude == true) 
-		{
-			require $paparFail;
-		}
-		else
-		{
-			if( in_array($nama,$cariNama) )
-				require $paparFail;	
-			elseif ( $nama == 'semak')
-				require $paparFail;	
-			else
-			{
-				require PAPAR . '/diatas.php';
-				require PAPAR . '/menu_atas.php';
-				require $paparFail;
-				require PAPAR . '/dibawah.php';	
-			}
-		}
-//*/
-	}
-#==========================================================================================
 #------------------------------------------------------------------------------------------
 	public function paparTemplate($nama, $template, $noInclude = false)
 	{
