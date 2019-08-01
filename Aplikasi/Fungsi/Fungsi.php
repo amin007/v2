ba47b29e474a50adbe572bcd22a44e7d6a907dd2
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-01 11:39:44 +0800

    buang space tab

--- Aplikasi/Fungsi/Fungsi.php
+++ Aplikasi/Fungsi/Fungsi.php
@@ -1,36 +1,36 @@
 <?php
-//namespace Aplikasi\Kitab; //echo __NAMESPACE__; 
+//namespace Aplikasi\Kitab; //echo __NAMESPACE__;
 #------------------------------------------------------------------------------------------
 function bulanan($jenis, $t)
-{    
-    if ($jenis=='kawalan') // $t = '12' , tahun
-        $bulan = array('rangka'.$t,
-        'jan'.$t, 'feb'.$t, 'mac'.$t, 'apr'.$t, 
-        'mei'.$t, 'jun'.$t, 'jul'.$t, 'ogo'.$t, 
-        'sep'.$t, 'okt'.$t, 'nov'.$t, 'dis'.$t);
-    elseif ($jenis=='data_bulanan') 
-        $bulan = array(
-        'jan'.$t, 'feb'.$t, 'mac'.$t, 'apr'.$t, 
-        'mei'.$t, 'jun'.$t, 'jul'.$t, 'ogo'.$t, 
-        'sep'.$t, 'okt'.$t, 'nov'.$t, 'dis'.$t);
-    elseif ($jenis=='nama_bulan') 
-        $bulan = array(
-        'jan', 'feb', 'mac', 'apr', 
-        'mei', 'jun', 'jul', 'ogo', 
-        'sep', 'okt', 'nov', 'dis');
-    elseif ($jenis=='key_nama_bulan') 
-        $bulan = array(
-        'jan' => 0, 'feb' => 1, 'mac' => 2, 'apr' => 3, 
-        'mei' => 4, 'jun' => 5, 'jul' => 6, 'ogo' => 7, 
-        'sep' => 8, 'okt' => 9, 'nov' =>10, 'dis' =>11);
-    elseif ($jenis=='bulan_penuh')
-        $bulan = array('Januari', 'Februari', 'Mac', 'April', 
-        'Mei', 'Jun', 'Julai', 'Ogos', 
-        'September', 'Oktober', 'November', 'Disember');
-   elseif ($jenis=='key_bulan_penuh')
-        $bulan = array('Januari' => 0, 'Februari' => 1, 'Mac' => 2, 'April' => 3, 
-        'Mei' => 4, 'Jun' => 5, 'Julai' => 6, 'Ogos' => 7, 
-        'September' => 8, 'Oktober' => 9, 'November' => 10, 'Disember' => 11);
+{
+	if ($jenis=='kawalan')// $t = '12' , tahun
+		$bulan = array('rangka'.$t,
+		'jan'.$t, 'feb'.$t, 'mac'.$t, 'apr'.$t,
+		'mei'.$t, 'jun'.$t, 'jul'.$t, 'ogo'.$t,
+		'sep'.$t, 'okt'.$t, 'nov'.$t, 'dis'.$t);
+	elseif ($jenis=='data_bulanan')
+		$bulan = array(
+		'jan'.$t, 'feb'.$t, 'mac'.$t, 'apr'.$t,
+		'mei'.$t, 'jun'.$t, 'jul'.$t, 'ogo'.$t,
+		'sep'.$t, 'okt'.$t, 'nov'.$t, 'dis'.$t);
+	elseif ($jenis=='nama_bulan')
+		$bulan = array(
+		'jan', 'feb', 'mac', 'apr',
+		'mei', 'jun', 'jul', 'ogo',
+		'sep', 'okt', 'nov', 'dis');
+	elseif ($jenis=='key_nama_bulan')
+		$bulan = array(
+		'jan' => 0, 'feb' => 1, 'mac' => 2, 'apr' => 3,
+		'mei' => 4, 'jun' => 5, 'jul' => 6, 'ogo' => 7,
+		'sep' => 8, 'okt' => 9, 'nov' =>10, 'dis' =>11);
+	elseif ($jenis=='bulan_penuh')
+		$bulan = array('Januari', 'Februari', 'Mac', 'April',
+		'Mei', 'Jun', 'Julai', 'Ogos',
+		'September', 'Oktober', 'November', 'Disember');
+	elseif ($jenis=='key_bulan_penuh')
+		$bulan = array('Januari' => 0, 'Februari' => 1, 'Mac' => 2, 'April' => 3,
+		'Mei' => 4, 'Jun' => 5, 'Julai' => 6, 'Ogos' => 7,
+		'September' => 8, 'Oktober' => 9, 'November' => 10, 'Disember' => 11);
 	elseif ($jenis=='nama_harian')
 		$bulan = array('Ahad','Isnin','Selasa','Rabu',
 		'Khamis','Jumaat','Sabtu');
@@ -44,24 +44,24 @@ function bulanan($jenis, $t)
 #------------------------------------------------------------------------------------------
 function dptNamaHari($tarikh)
 {
-	$hb = date('w', strtotime($tarikh)); // w = cari key dalam tarikh
-	$namaHari = array_search($hb,bulanan('key_nama_harian', null)); // papar nama hari
+	$hb = date('w', strtotime($tarikh)); # w = cari key dalam tarikh
+	$namaHari = array_search($hb,bulanan('key_nama_harian', null)); # papar nama hari
 	return $namaHari;
 }
 #------------------------------------------------------------------------------------------
-// mula untuk kod php+html 
+// mula untuk kod php+html
 function papar_jadual($row, $myTable, $pilih)
 {
-	if ($pilih == 1) 
+	if ($pilih == 1)
 	{
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-		?><!-- Jadual <?php echo $myTable ?> -->	
+		?><!-- Jadual <?php echo $myTable ?> -->
 		<table  border="1" class="excel" id="example"><?php
 		// mula bina jadual
 		$printed_headers = false; 
 		#-----------------------------------------------------------------
 		for ($kira=0; $kira < count($row); $kira++)
-		{	//print the headers once: 	
+		{	//print the headers once:
 			if ( !$printed_headers ) : ?>
 		<thead><tr>
 		<th>#</th><?php foreach ( array_keys($row[$kira]) as $tajuk ) :
@@ -202,7 +202,7 @@ function kira($kiraan)
 {
 	// pecahan kepada ratus
 	return number_format($kiraan,0,'.',',');
-} 
+}
 #------------------------------------------------------------------------------------------
 function kira2($dulu,$kini)
 {
@@ -211,12 +211,12 @@ function kira2($dulu,$kini)
 	//@$kiraan=(($kini-$dulu)/$dulu)*100;
 }
 #------------------------------------------------------------------------------------------
-function kira3($kira,$n) 
+function kira3($kira,$n)
 {
 	return str_pad($kira,$n,'0',STR_PAD_LEFT);
 }
 #------------------------------------------------------------------------------------------
-function huruf($jenis, $papar) 
+function huruf($jenis, $papar)
 {
     /*
 		huruf('BESAR', )
@@ -224,34 +224,32 @@ function huruf($jenis, $papar)
 		huruf('Depan', )
 		huruf('Besar_Depan', )
     */
-    
-    switch ($jenis) 
-    {// mula - pilih $jenis
-    case 'BESAR':
-        $papar = strtoupper($papar);
-        break;
-    case 'kecil':
-        $papar = strtolower($papar);
-        break;
-    case 'Depan':
-        $papar = ucfrist($papar);
-        break;
-    case 'Besar_Depan':
-        $papar = mb_convert_case($papar, MB_CASE_TITLE);
-        break;
-    }// tamat - pilih $jenis
-	
-	return $papar;
+	switch($jenis)
+	{# mula - pilih $jenis
+		case 'BESAR':
+			$papar = strtoupper($papar);
+			break;
+		case 'kecil':
+			$papar = strtolower($papar);
+			break;
+		case 'Depan':
+			$papar = ucfrist($papar);
+			break;
+		case 'Besar_Depan':
+			$papar = mb_convert_case($papar, MB_CASE_TITLE);
+			break;
+	}# tamat - pilih $jenis
 
+	return $papar;
 }
 #------------------------------------------------------------------------------------------
-function bersih($papar) 
+function bersih($papar)
 {
 	# lepas lari aksara khas dalam SQL
 	//$papar = mysql_real_escape_string($papar);
-	# buang ruang kosong (atau aksara lain) dari mula & akhir 
+	# buang ruang kosong (atau aksara lain) dari mula & akhir
 	$papar = trim($papar);
-	
+
 	return $papar;
 }
 #------------------------------------------------------------------------------------------
@@ -260,14 +258,14 @@ function gambar_latarbelakang()
 	$tmpt2 = ($_SERVER['SERVER_NAME']=='www.amin007.org') ?
 		'../bssu/bg/bg' // utk website amin007
 		: '../../../private_html/bg/bg'; // utk localhost
-		
-    foreach(scandir($tmpt2) as $gambar) 
-    {
-        if (substr($gambar,-3) == 'jpg') 
-            $papar[]=$gambar;
-    }
-      
-    $today = rand(0, count($papar)-1); 
-    return $papar[$today];
+
+	foreach(scandir($tmpt2) as $gambar)
+	{
+		if (substr($gambar,-3) == 'jpg')
+			$papar[]=$gambar;
+	}
+
+	$today = rand(0, count($papar)-1);
+	return $papar[$today];
 }
 #------------------------------------------------------------------------------------------
