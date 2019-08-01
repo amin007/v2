afecd94333b8d9d265e5919ec7488f5813327b38
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-01 13:02:23 +0800

    contoh include template

---
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin000.php
@@ -0,0 +1,11 @@
+<form class="bg-light">
+	<div class="form-group">
+		<label for="a" class="text-info">Selamat datang ke halaman kami</label>
+	</div>
+	<div class="form-group">
+		<label for="b" class="text-dark">Macam mana kehidupan anda pada hari ini</label>
+	</div>
+	<div class="form-group">
+		<label for="c" class="text-success">Semoga anda ceria sepanjang masa</label>
+	</div>
+</form>
---
+++ sumber/fail/php/template/bootstrap/4.3.1/borangLogin001.php
@@ -0,0 +1,17 @@
+<form>
+	<div class="form-group">
+		<label for="exampleInputEmail1">Email</label>
+		<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan Email">
+		<small id="emailHelp" class="form-text text-muted">Kita berjanji tidak akan berkongsi kasih<br>
+		eh salah berkongsi email dengan orang lain.</small>
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Rahsia Antara Kita</label>
+		<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+	</div>
+	<div class="form-group form-check">
+		<input type="checkbox" class="form-check-input" id="exampleCheck1">
+		<label class="form-check-label" for="exampleCheck1">Kenang daku dalam doamu</label>
+	</div>
+	<input type="submit" class="btn btn-primary btn-block" value="Hantar">
+</form>
