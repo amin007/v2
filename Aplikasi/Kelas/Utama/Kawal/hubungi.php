4a2815bda9733d194c47329bb843f3aa6583d2f5
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-06 22:33:13 +0800

    tambah class Hubungi

---
+++ Aplikasi/Kelas/Utama/Kawal/hubungi.php
@@ -0,0 +1,67 @@
+<?php
+namespace Aplikasi\Kawal; //echo __NAMESPACE__;
+class Hubungi extends \Aplikasi\Kitab\Kawal
+{
+#==========================================================================================
+	function __construct()
+	{
+		parent::__construct();
+		//\Aplikasi\Kitab\Kebenaran::kawalMasuk();
+		//\Aplikasi\Kitab\Kebenaran::kawalKeluar();
+		$this->_folder = huruf('kecil', namaClass($this));
+		//echo '<hr>Nama class :' . __METHOD__ . '<hr>';
+		//echo '<hr>Nama function :' .__FUNCTION__ . '<hr>';
+	}
+##------------------------------------------------------------------------------------------
+	public function index($a=null)
+	{
+		# Set pemboleubah utama
+		//$this->papar->Tajuk_Muka_Surat = $tajuk;
+		//echo '<hr>Nama class :' . __METHOD__ . '<hr>';
+
+		# Pergi papar kandungan
+		$this->paparHeader('hubungi/kelabpeminat');
+		//$this->semakPembolehubah($this->papar->senarai,'senarai');# Semak data dulu
+		//$this->paparKandungan($this->_folder,'index',$noInclude=0);
+	}
+##------------------------------------------------------------------------------------------
+	public function paparHeader($lokasi = 'pergi/mana')
+	{
+		//$this->paparHeader('sumber/fail/rujukan');
+		////$this->semakPembolehubah($ujian,'ujian',0);# Semak data dulu
+		//echo '<br>location: ' . URL . $lokasi;
+		header('location:' . URL . $lokasi);//*/
+	}
+##------------------------------------------------------------------------------------------
+	public function paparKandungan($folder, $fail, $noInclude)
+	{
+		$jenis = $this->papar->pilihTemplate($template=431);
+		$this->papar->bacaTemplate(
+		//$this->papar->paparTemplate(
+			$this->_folder . '/' . $fail, $jenis, $noInclude); # $noInclude=0
+			//'mobile/mobile',$jenis,0); # $noInclude=0
+		//*/
+	}
+##------------------------------------------------------------------------------------------
+	function logout()
+	{
+		//echo '<pre>sebelum:'; print_r($_SESSION); echo '</pre>';
+		\Aplikasi\Kitab\Sesi::destroy();
+		header('location: ' . URL);
+		//exit;
+	}
+#==========================================================================================
+#------------------------------------------------------------------------------------------
+	public function kelabpeminat($a=null)
+	{	
+		# Set pemboleubah utama
+		$this->papar->Tajuk_Muka_Surat = $a;
+		echo '<hr>Nama class :' . __METHOD__ . '<hr>';
+		
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->senarai,'senarai');# Semak data dulu
+		//$this->paparKandungan($this->_folder,'index',$noInclude=0);
+	}
+#------------------------------------------------------------------------------------------
+#==========================================================================================
+}
