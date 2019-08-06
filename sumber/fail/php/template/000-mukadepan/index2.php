85f3b00695883f58e803a89aa22f5fe6db307e3d
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-07 01:33:15 +0800

    tambah gambar latarbelakang lain

--- sumber/fail/php/template/000-mukadepan/index2.php
+++ sumber/fail/php/template/000-mukadepan/index2.php
@@ -13,7 +13,8 @@
 <style type="text/css">
 html, body {
 	height: 100%;
-	background: url("https://cdn.pixabay.com/photo/2015/09/04/19/32/landscape-922581_960_720.png")
+	background: url("https://cdn.nohat.cc/thumb/f/720/4530650874904576.jpg")
+	/*background: url("https://cdn.pixabay.com/photo/2015/09/04/19/32/landscape-922581_960_720.png")*/
 	no-repeat center center fixed;background-size: cover;;
 }
 body {
