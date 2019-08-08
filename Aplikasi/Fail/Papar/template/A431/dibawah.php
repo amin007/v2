5e13aefbdea5fb8cc0863f7bffe246c4fd00aad2
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-09 00:52:58 +0800

    guna navbar berbanding dengan footer

--- Aplikasi/Fail/Papar/template/A431/dibawah.php
+++ Aplikasi/Fail/Papar/template/A431/dibawah.php
@@ -1,11 +1,18 @@
 <?php echo "\n"; ?><!-- Footer
 ================================================== -->
+<nav class="navbar navbar-light">
+	<span class="label label-info bg-secondary text-white">&copy; Hak Cipta Terperihara 2019. Theme <?php
+$theme = ( !isset($pilih) ) ? 'Asal Bootstrap Twitter' : $pilih;
+echo $theme = (isset($theme)) ? $theme : null; ?> </span>
+	<span class="label label-info bg-secondary text-white">PHP version: <?php print phpversion(); ?></span>
+</nav>
 <!-- footer class="footer">
 	<div class="container">
 		<span class="label label-info">
-		&copy; Hak Cipta Terperihara 2016. Theme <?php 
+		&copy; Hak Cipta Terperihara 2019. Theme <?php
 $theme = ( !isset($pilih) ) ? 'Asal Bootstrap Twitter' : $pilih;
-echo $theme = (isset($theme)) ? $theme : null; ?>	
+echo $theme = (isset($theme)) ? $theme : null; ?><br>
+		PHP version: <?php print phpversion(); ?><br>
 		</span>
 	</div>
 </footer -->
