18cb2fedf47a705e06da01041b3e05bb5b57567c
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-09 08:46:11 +0800

    tambah bilangan versi untuk dasar privasi

--- Aplikasi/Fail/Papar/index/privasi.php
+++ Aplikasi/Fail/Papar/index/privasi.php
@@ -1,12 +1,19 @@
 <div class="container bg-light text-dark">
-<h1>Dasar Privasi</h1>
-<p><a class="bg-secondary text-white">amin007.org</a> menghormati pelawat privasi dan ia adalah penting bagi kami. Kenyataan berikut menerangkan dasar privasi <a class="bg-secondary text-white">amin007.org</a> . Dasar privasi ini digunakan untuk laman web ini dan semua produk, kandungan & perkhidmatan yang ditawarkan oleh pihak kami.</p>
+<h1>Dasar Privasi <small>versi 1.0.0</small></h1>
+<p><a class="bg-secondary text-white">amin007.org</a> menghormati pelawat privasi dan ia adalah penting bagi kami.
+Kenyataan berikut menerangkan dasar privasi <a class="bg-secondary text-white">amin007.org</a> .
+Dasar privasi ini digunakan untuk laman web ini dan semua produk, kandungan & perkhidmatan yang ditawarkan oleh pihak kami.</p>
 
 <b>Maklumat Pengenalan Peribadi:</b>
-<p><a class="bg-secondary text-white">amin007.org</a> tidak memerlukan pendaftaran pengguna. Pengguna bebas untuk melawat di <a class="bg-secondary text-white">amin007.org</a> , menavigasi semua halamannya, membaca semua tutorial, melihat demo dan muat turun skrip. Kami mengumpul maklumat peribadi apabila pengunjung mengisi borang, melanggan surat berita kami dan cuba demo tutorial di laman web kami.</p>
+<p><a class="bg-secondary text-white">amin007.org</a> tidak memerlukan pendaftaran pengguna.
+Pengguna bebas untuk melawat di <a class="bg-secondary text-white">amin007.org</a> , menavigasi semua halamannya,
+membaca semua tutorial, melihat demo dan muat turun skrip.
+Kami mengumpul maklumat peribadi apabila pengunjung mengisi borang,
+melanggan surat berita kami dan cuba demo tutorial di laman web kami.</p>
 
 <b>Log:</b>
-<p>Setiap kali anda mengakses laman web kami, <a class="bg-secondary text-white">amin007.org</a> mengekalkan fail log. Maklumat berikut disimpan dalam fail log.</p>
+<p>Setiap kali anda mengakses laman web kami, <a class="bg-secondary text-white">amin007.org</a> mengekalkan fail log.
+Maklumat berikut disimpan dalam fail log.</p>
 	<ul>
 	<li>Alamat IP anda,</li>
 	<li>Pembekal Perkhidmatan Internet,</li>
@@ -18,26 +25,39 @@
 	</ul>
 
 <b>Pautan ke Laman Web Lain:</b>
-<p><a class="bg-secondary text-white">amin007.org</a> mengandungi banyak pautan ke laman web lain yang tidak dimiliki atau dikendalikan oleh Kami. Kami tidak bertanggungjawab untuk privasi laman web pihak ketiga sedemikian dan privasi kami tidak mengawal anda untuk menggunakan laman web yang lain. Dasar kami hanya terpakai kepada maklumat yang dikumpul oleh <a class="bg-secondary text-white">amin007.org</a>. Kami meminta anda supaya sedar sebelum meninggalkan tapak kami dan membaca dasar privasi laman web pihak ketiga tersebut.</p>
+<p><a class="bg-secondary text-white">amin007.org</a> mengandungi banyak pautan ke laman web lain yang tidak dimiliki atau
+dikendalikan oleh Kami. Kami tidak bertanggungjawab untuk privasi laman web pihak ketiga sedemikian dan privasi kami tidak
+mengawal anda untuk menggunakan laman web yang lain. Dasar kami hanya terpakai kepada maklumat yang dikumpul oleh
+<a class="bg-secondary text-white">amin007.org</a>. Kami meminta anda supaya sedar sebelum meninggalkan tapak kami dan
+membaca dasar privasi laman web pihak ketiga tersebut.</p>
 
 <b>Kuki:</b>
-<p>Laman kami menggunakan "kuki" untuk meningkatkan pengalaman pelawat dan meningkatkan kelajuan laman web kami. Kami tidak menggunakan kuki untuk mengumpul sebarang maklumat peribadi atau maklumat sensitif.
-Kuki di <a class="bg-secondary text-white">amin007.org</a> digunakan oleh pihak kami dan vendor pihak ketiga kita. Kuki digunakan untuk sebab berikut.</p>
+<p>Laman kami menggunakan "kuki" untuk meningkatkan pengalaman pelawat dan meningkatkan kelajuan laman web kami.
+Kami tidak menggunakan kuki untuk mengumpul sebarang maklumat peribadi atau maklumat sensitif.
+Kuki di <a class="bg-secondary text-white">amin007.org</a> digunakan oleh pihak kami dan vendor pihak ketiga kita.
+Kuki digunakan untuk sebab berikut.</p>
 	<ul>
-	<li>Penjual pihak ketiga, termasuk Nuffnang, menggunakan kuki untuk menayangkan iklan berdasarkan kunjungan sebelumnya ke laman web ini.</li>
-	<li>Penggunaan Google bagi kuki Nuffnang membolehkannya dan rakannya untuk memberikan iklan kepada anda berdasarkan kunjungan anda ke laman web ini.</li>
+	<li>Penjual pihak ketiga, termasuk Nuffnang, menggunakan kuki untuk menayangkan iklan berdasarkan kunjungan sebelumnya
+	ke laman web ini.</li>
+	<li>Penggunaan Google bagi kuki Nuffnang membolehkannya dan rakannya untuk memberikan iklan kepada anda berdasarkan
+	kunjungan anda ke laman web ini.</li>
 	<li>Anda boleh memilih untuk tidak menggunakan kuki Nuffnang untuk pengiklanan berdasarkan minat dengan melawat Pengaturan Iklan.
-	(Sebagai alternatif, anda boleh mengarahkan pengguna untuk memilih keluar dari kuki penggunaan vendor pihak ketiga untuk pengiklanan berdasarkan minat dengan melawat
+	(Sebagai alternatif, anda boleh mengarahkan pengguna untuk memilih keluar dari kuki penggunaan vendor pihak ketiga
+	untuk pengiklanan berdasarkan minat dengan melawat
 	<a href="nuffnang.com.my" class="bg-info text-white">nuffnang.com.my</a>)
 	</li>
 	</ul>
 <p>Selain itu, kami menggunakan kuki untuk mengumpul statistik pelawat dan ini membantu kami memperbaiki laman web kami.</p>
-<p><span class="bg-warning">Google Analytics</span> - Kami menggunakan <span class="bg-warning">Google Analytics</span> untuk melayari pengunjung dan statistik trafik laman web.
-Ia mungkin mengumpul lokasi pelawat, IP atau sebarang maklumat lain. Dasar Privasi <span class="bg-warning">Google Analytics</span> dimiliki oleh Google.
+<p><span class="bg-warning">Google Analytics</span> - Kami menggunakan <span class="bg-warning">Google Analytics</span>
+untuk melayari pengunjung dan statistik trafik laman web.
+Ia mungkin mengumpul lokasi pelawat, IP atau sebarang maklumat lain.
+Dasar Privasi <span class="bg-warning">Google Analytics</span> dimiliki oleh Google.
 </p>
 
 <b>Penerimaan Dasar Privasi anda:</b>
-<p>Dengan menggunakan laman web ini, anda bersetuju dengan dasar ini. Jika anda tidak bersetuju dengan dasar ini, sila jangan mengakses laman web kami.
-Sekiranya anda mahukan maklumat lain atau mempunyai sebarang pertanyaan mengenai dasar privasi kami, sila hubungi kami melalui e-mel di al.amin007[at]yahoo.com.
+<p>Dengan menggunakan laman web ini, anda bersetuju dengan dasar ini. Jika anda tidak bersetuju dengan dasar ini,
+sila jangan mengakses laman web kami.
+Sekiranya anda mahukan maklumat lain atau mempunyai sebarang pertanyaan mengenai dasar privasi kami,
+sila hubungi kami melalui e-mel di al.amin007[at]yahoo[dot]com.
 </p>
 </div><!-- / class="container bg-light text-dark" -->
