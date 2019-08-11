7bebd8c8a180e5c830d451afdb1e325e3d1cdaa1
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-11 20:36:38 +0800

    tambha pautam ke media sosial

---
+++ Aplikasi/Fail/Papar/hubungi/mediasosial.php
@@ -0,0 +1,17 @@
+<hr>
+<h1>&nbsp;<span class="bg-secondary">&nbsp;<?php echo $this->Tajuk_Muka_Surat; ?>&nbsp;</span></h1>
+<div class="row">
+	<div class="col-lg-3">&nbsp;</div>
+	<div class="col-lg-6">
+		<blockquote>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-facebook"></i>: amin007.ledang</a>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-twitter-square"></i>: amin007_ledang</a>
+			<a href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary text-white"><i class="fab fa-linkedin"></i>: amin007ledang</a>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-github"></i>: amin007</a>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-instagram"></i>: aminledang</a>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-skype"></i>: amin800507</a>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-snapchat">amin007ledang</i></a>
+			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-youtube"></i>: user/abdmuhaimin</a>
+		</blockquote>
+	</div>
+</div>
