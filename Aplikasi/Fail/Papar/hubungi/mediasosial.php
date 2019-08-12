6cd988241c9f8a6c48f833e517629fa10d25ccf8
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-12 13:18:47 +0800

    tambha target _blank

--- Aplikasi/Fail/Papar/hubungi/mediasosial.php
+++ Aplikasi/Fail/Papar/hubungi/mediasosial.php
@@ -8,17 +8,17 @@
 				<i class="fab fa-facebook"></i>: amin007.ledang</a>
 			<a target="_blank" href="https://www.twitter.com/amin007_ledang" class="btn btn-secondary text-white">
 				<i class="fab fa-twitter-square"></i>: amin007_ledang</a>
-			<a href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary text-white">
+			<a target="_blank" href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary text-white">
 				<i class="fab fa-linkedin"></i>: amin007ledang</a>
-			<a href="#" class="btn btn-secondary text-white">
+			<a target="_blank" href="#" class="btn btn-secondary text-white">
 				<i class="fab fa-github"></i>: amin007</a>
-			<a href="#" class="btn btn-secondary text-white">
+			<a target="_blank" href="#" class="btn btn-secondary text-white">
 				<i class="fab fa-instagram"></i>: aminledang</a>
-			<a href="#" class="btn btn-secondary text-white">
+			<a target="_blank" href="#" class="btn btn-secondary text-white">
 				<i class="fab fa-skype"></i>: amin800507</a>
-			<a href="#" class="btn btn-secondary text-white">
+			<a target="_blank" href="#" class="btn btn-secondary text-white">
 				<i class="fab fa-snapchat">amin007ledang</i></a>
-			<a href="#" class="btn btn-secondary text-white">
+			<a target="_blank" href="#" class="btn btn-secondary text-white">
 				<i class="fab fa-youtube"></i>: user/abdmuhaimin</a>
 		</blockquote>
 	</div>
