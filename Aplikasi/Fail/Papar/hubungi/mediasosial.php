ee65574296124c5b2820d95394e1819dbd2789d0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-12 13:10:30 +0800

    susun semua pautan

--- Aplikasi/Fail/Papar/hubungi/mediasosial.php
+++ Aplikasi/Fail/Papar/hubungi/mediasosial.php
@@ -4,14 +4,22 @@
 	<div class="col-lg-3">&nbsp;</div>
 	<div class="col-lg-6">
 		<blockquote>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-facebook"></i>: amin007.ledang</a>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-twitter-square"></i>: amin007_ledang</a>
-			<a href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary text-white"><i class="fab fa-linkedin"></i>: amin007ledang</a>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-github"></i>: amin007</a>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-instagram"></i>: aminledang</a>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-skype"></i>: amin800507</a>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-snapchat">amin007ledang</i></a>
-			<a href="#" class="btn btn-secondary text-white"><i class="fab fa-youtube"></i>: user/abdmuhaimin</a>
+			<a target="_blank" href="https://www.facebook.com/amin007.ledang" class="btn btn-secondary text-white">
+				<i class="fab fa-facebook"></i>: amin007.ledang</a>
+			<a href="#" class="btn btn-secondary text-white">
+				<i class="fab fa-twitter-square"></i>: amin007_ledang</a>
+			<a href="https://www.linkedin.com/in/amin007ledang" class="btn btn-secondary text-white">
+				<i class="fab fa-linkedin"></i>: amin007ledang</a>
+			<a href="#" class="btn btn-secondary text-white">
+				<i class="fab fa-github"></i>: amin007</a>
+			<a href="#" class="btn btn-secondary text-white">
+				<i class="fab fa-instagram"></i>: aminledang</a>
+			<a href="#" class="btn btn-secondary text-white">
+				<i class="fab fa-skype"></i>: amin800507</a>
+			<a href="#" class="btn btn-secondary text-white">
+				<i class="fab fa-snapchat">amin007ledang</i></a>
+			<a href="#" class="btn btn-secondary text-white">
+				<i class="fab fa-youtube"></i>: user/abdmuhaimin</a>
 		</blockquote>
 	</div>
 </div>
