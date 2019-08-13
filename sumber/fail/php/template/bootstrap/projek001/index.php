0213fe0df0028e81efd245d798c85e3c2d4ac017
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-13 17:27:59 +0800

    ubahsuai <br>

--- sumber/fail/php/template/bootstrap/projek001/index.php
+++ sumber/fail/php/template/bootstrap/projek001/index.php
@@ -172,34 +172,32 @@
 <tr>
 <td>&nbsp;</td>
 <td>
-	<!-- ########################################### -->
 	<i class="fas fa-user"></i>
 	<h5><small class="text-muted">Langkah 1</small><br>
 	Siapa nama anda?</h5>
-	<!-- ########################################### -->
+	<br><br><br><br><br>
 	<i class="fas fa-users"></i>
 	<h5><small class="text-muted">Langkah 2</small><br>
 	Penyertaan</h5>
-	<!-- ########################################### -->
+	<br><br><br><br><br>
 	<i class="fab fa-fort-awesome"></i>
 	<h5><small class="text-muted">Langkah 3</small><br>
 	Tema Acara</h5>
-	<!-- ########################################### -->
+	<br><br><br><br><br>
 	<i class="far fa-calendar-alt"></i>
 	<h5><small class="text-muted">Langkah 4</small><br>
 	Tarikh</h5>
-	<!-- ########################################### -->
+	<br><br><br><br><br>
 	<i class="fas fa-map-marker"></i>
 	<h5><small class="text-muted">Langkah 5</small><br>
 	Lokasi</h5>
-	<!-- ########################################### -->
 </td>
 <td class="text-left">
 	<div class="dotted">
 		<div class="icon"><i class="fa fa-camera-retro"></i></div>
-		<br><br><br><br><br>
+		<br><br><br><br><br><br>
 		<div class="icon"><i class="fa fa-camera-retro"></i></div>
-		<br><br><br><br><br>
+		<br><br><br><br><br><br>
 		<div class="icon"><i class="far fa-circle"></i></div>
 	</div>
 </td>
