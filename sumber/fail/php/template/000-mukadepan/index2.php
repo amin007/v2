f1801af6591a3230d9ea695acb3e1d2dfda398b5
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-08-13 23:15:50 +0800

    tambah link baru

--- sumber/fail/php/template/000-mukadepan/index2.php
+++ sumber/fail/php/template/000-mukadepan/index2.php
@@ -203,7 +203,8 @@ table.excel tbody td
 
 <!-- khas untuk jquery dan js2 lain
 ================================================== -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
