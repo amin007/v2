0c27cfbf271e96bef20581b59f5e35c6796af0ea
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-29 11:14:52 +0800

    ubah warna latar belakang

--- Aplikasi/Fail/Papar/sesat/index.php
+++ Aplikasi/Fail/Papar/sesat/index.php
@@ -1,8 +1,9 @@
-<div class="container">
+<!-- div class="container" style="background-color:white;opacity:.70;-moz-opacity:.70;filter:alpha(opacity=70);" -->
+<div class="container bg-secondary text-white">
 <div class="hero-unit">
 <h1>Sesat daa</h1>
 <p><?php echo $this->mesej; ?></p>
-<pre><?php
+<pre class="text-white"><?php
 $url = dpt_url_xfilter();
 if( isset($url) )
 {
