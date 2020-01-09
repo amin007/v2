cb5cd346b9987b8a7220255f874703e8fb855238
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-01-09 09:32:44 +0800

    nota 02 guru bisnes

---
+++ sumber/fail/txt/seminar/limagurubisnes02.php
@@ -0,0 +1,45 @@
+#ceritaharikhamis #pelaburan
+Bismillah. Seminar di malaysia - bahagian 2.
+
+Kenapa kita perlu sertai seminar?
+
+Sebelum itu kita merujuk kepada website dewan bahasa dan pustaka untuk mengetahui maksud
+persidangan, seminar, bengkel dan simposium.
+
+[per.si.da.ngan] | ڤرسيدڠن
+Definisi : perjumpaan (utk membincangkan sesuatu), mesyuarat, rapat: masing-masing yg hadir dlm ~ tersebut mempunyai tafsir­annya tersendiri terhadap dasar itu; Majlis Keselamatan Bangsa-Bangsa Bersatu telah menangguhkan ~ yg sedang membahaskan krisis di Timur Tengah; (Kamus Dewan Edisi Keempat)
+
+[se.mi.nar]
+Definisi : (séminar) perjumpaan utk berbincang atau bertukar-tukar pendapat tentang perkara-perkara tertentu: pd tahun lepas, ia telah pergi menghadiri ~ utk membincangkan kedudukan kaum wanita; berseminar mengadakan seminar, meng­hadiri (menyertai) seminar: janganlah kita asyik ~ tanpa usaha lanjutan; menseminarkan membahaskan sesuatu per-kara, isu, dsb dlm seminar: perkara itu perlulah diseminarkan terlebih dahulu. (Kamus Dewan Edisi Keempat)
+
+[béng.kél] | بيڠکيل
+Definisi : (béngkél) 1. tempat membaiki kereta dll: syarikat kerjasama kenderaan Melayu Melaka juga sudah mempunyai ~-~ tempat membaiki kereta; 2. kilang kecil (tempat tukang-tukang bekerja); 3. mesyuarat (perbincangan dsb) secara berkelompok utk mengkaji sesuatu atau bertukar-tukar pendapat (idea dsb) tentang sesuatu: ~ penulisan cerita kanak-kanak;berbengkel mengadakan, menghadiri atau menyertai bengkel (perbincangan berkelompok): mereka sering ~, berseminar dsb utk menyiapkan projek penulisan itu;membengkelkan membincangkan sesuatu dlm perbincangan berkelompok; perbengkelan urusan atau aktiviti berbengkel. (Kamus Dewan Edisi Keempat)
+
+[sim.po.sium] | سيمڤوسيوم
+Definisi : pertemuan antara para cendekia­wan, pakar, dsb utk membincangkan sesuatu persoalan, perkara, dsb; bersimposium menghadiri, menyertai atau mengadakan simposium: memang ada orang yg suka berseminar, berbengkel, ~ dsb di sana sini. (Kamus Dewan Edisi Keempat)
+
+Sebagai orang yang pertama kali buat bisnes, biasanya kita akan buat sahaja benda yang kita pandai.
+
+Sampai satu tahap bisnes kita sudah maju dan ada masalah-masalah yang datang satu demi satu.
+
+Hendak tanya kawan-kawan, mereka pun bukan orang yang terlibat dalam bisnes pun, walaupun kita dapat nasihat percuma.
+
+Apabila kita laksanakan idea dari nasihat percuma tersebut, masalah lain yang muncul.
+
+Setelah buat kajian di google dan facebook dan whatsapp, kita pun cuba ambil nasihat berbayar.
+
+Namum apabila kita laksanakan idea dari nasihat berbayar, masalah lain yang timbul.
+
+Kata orang tua, kita banyak langgar dinding batu berkali-kali.
+
+Akhirnya kita terbaca seseorang dari Kelantan ingin buat seminar tentang pengalaman 5 tokoh yang terlibat dalam bisnes.
+
+Tokoh-tokoh yang disebutkan kita sudah ikuti perkembangan di media sosial.
+
+Kita tengah pertimbangkan kos masa dan duit untuk pergi ke seminar tersebut.
+
+Kempen sudah bermula dari bulan November 2019, manakala tarikh seminar pada bulan Januari 2020.
+
+Persoalan di kepala, adalah berbaloi pergi ke seminar tersebut sedangkan kita boleh dapat maklumat yang diperlukan melalui media sosial sahaja?
+
+Bersambung...
