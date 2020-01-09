42fa13e8a804234a210f2b92be6786aeeb5cc8d7
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-01-09 11:27:40 +0800

    tambah nota tambahan

--- sumber/fail/txt/seminar/limagurubisnes03.php
+++ sumber/fail/txt/seminar/limagurubisnes03.php
@@ -1,4 +1,42 @@
 #ceritaharikhamis #pelaburan
 Bismillah. Seminar di malaysia - bahagian 3.
 
-Tarikh dan masa sesuai untuk daftar seminar. 
+Tarikh dan masa sesuai untuk daftar seminar.
+
+Kebiasaan di malaysia, ada erley bird untuk tiket seminar.
+
+Dia orang boleh tawarkan sehingga 50% diskaun.
+
+Jika harga yahudi RM 5000, kita boleh dapat pada harga RM 2500.
+
+Kalau kita ini kenalan lama, kita boleh dapat pada harga RM 1250.
+
+Kempen sudah bermula dari bulan November 2019, manakala tarikh seminar pada bulan Januari 2020.
+
+Persoalan di kepala, adalah berbaloi pergi ke seminar tersebut sedangkan kita boleh dapat maklumat yang diperlukan melalui media sosial sahaja?
+
+Jika kita ada duit, masa pula tidak sesuai.
+
+Jika kita ada masa, duit pula tidak sesuai.
+
+JIka kita ada masa dan duit, mood pula tidak sesuai.
+
+Oh bermain dengan mood juga ya pergi seminar?
+
+Biasanya dalam seminar ada maklumat khusus yang tidak didedahkan kepada awam.
+
+Kita sangka semua maklumat boleh dapat atas talian internet.
+
+Namun ada wujud juga dark web juga.
+
+Ajak member pergi mengeteh atau minum kopi(istilah bisnes), itu pun tidak semua maklumat kita boleh capai.
+
+Ada risiko yang tidak dapat dijangka dalam mencari maklumat.
+
+Seperti Mission Imposible Fallout gitu.
+
+Jika kita takut, misi tidak dapat dilaksanakan.
+
+Oleh itu, kita perlu berubah mengikut keadaan semasa dan menyerap segala risiko.
+
+Bersambung...
--- sumber/fail/txt/seminar/limagurubisnes04.php
+++ sumber/fail/txt/seminar/limagurubisnes04.php
@@ -2,3 +2,79 @@
 Bismillah. Seminar di malaysia - bahagian 4.
 
 Pengembaraan pergi seminar pada hari kejadian.
+
+Pada hari sabtu, 4 Januari 2020 ada kursus haji di masjid jamek sultan ibraham.
+
+Baru masuk minggu ke 4 katanya.
+
+Entah macam mana boleh habis awal pada hari tersebut.
+
+Jadi kita bertolak awal ke Bandaraya Kuala Lumpur.
+
+Sampai di TBS, terus gerak ke LRT Bandar Tun Razak.
+
+Sampai di LRT Bandar Tun Razak, cari kedai makan.
+
+Pilih hidangan nasi, sayur dan telur serta air sirap panas.
+
+Tauke kedai tulis harga RM 4.00, murah betul.
+
+Selesai makan, kita berjalan-jalan di sekitar LRT tersebut.
+
+Terjumpa kedai gunting rambut pula, harganya dalam RM 9.00
+
+Selesai gunting rambut, kawan lama telefon pula.
+
+Ikut kawan lama jalan-jalan cari makan.
+
+Lepas itu singgah di Bukit Bintang.
+
+Masa itu berfikir hotel mana hendak bermalam pada hari tersebut.
+
+Tiba-tiba teringat wujud hotel Mcd berhampiran dengan masjid jamek Kuala Lumpur.
+
+Jika jalan kaki macam jauh sangat dari bukit bintang.
+
+Jadi kita naik monoril bukit bintang dan singgah di monoril hang tuah.
+
+Dari situ kita naik LRT hang tuah dan berhenti di LRT masjid jamek.
+
+Hotel Mcd dekat sangat di masjid jamek kuala lumpur.
+
+Harga dalam RM 90 campur deposit RM 50.
+
+Kawan lama telefon dan katanya hendak belanja pizza mamak aka roti canai.
+
+Namum tunggu punya tunggu, perut sudah berbunyi.
+
+Terpaksalah makan set nasi ayam KFC yang harganya RM 11.20 sahaja malam tersebut.
+
+Milo kita simpan untuk esok hari, syukur tidak minta ais lebih.
+
+Jika tidak, boleh beku milo jika simpan sampai pagi.
+
+Bilik hotel mcd sejuk sangat katanya, macam Frozen 2.
+
+Pagi 5 Januari 2020, kita terbangun awal.
+
+Teringat ada sahabat YBK akan edarkan makanan di masjid jamek kuala lumpur selepas solat subuh.
+
+Melihat kejap fakir miskin beratur mengambil makanan selepas solat subuh.
+
+Insaf kejap di pagi hari yang hening ini.
+
+Lepas itu singgah kejap di Mcd cari nasi lemak.
+
+Dia orang tengah masak daa, terpaksalah sarapan milo semalam.
+
+Namun rezeki dari Allah tidak pernah salah alamat.
+
+Ada pemandu grabcar telefon hendak belanja makan capati di kawasan LRT Dato' Keramat.
+
+Jauh naik LRT Masjid Jamek ke Lrt Dato' Keramat, lalu terorong mana entah.
+
+Dapatlah merasa capati yang paling sedap setakat ini, itu pun sebab orang belanja makan.
+
+
+
+
