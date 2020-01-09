b9f234222ce04a28d789f0583cda59b5ae6fb4a2
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-01-09 14:31:40 +0800

    tambah nota baru

--- sumber/fail/txt/seminar/limagurubisnes08.php
+++ sumber/fail/txt/seminar/limagurubisnes08.php
@@ -2,3 +2,41 @@
 Bismillah. Seminar di malaysia - bahagian 8.
 
 Masa rehat dan networking aka sosial.
+
+Tempat networking orang perempuan biasanya dalam tandas awam pun jadi katanya.
+
+Tempat networking orang lelaki biasanya di meja makan.
+
+Jika kita jumpa kawan lama atau tokoh dalam tandas, kita hanya lambai tangan sahaja.
+
+Hendak bagi salam macam tidak sesuai, apatah lagi selfie dalam tandas.
+
+Masuk dewan tengok ramai orang buat networking sesama sendiri.
+
+Jumpa Dato' Roslan dan bersalam dengan beliau.
+
+Dato' Roslan ucapkan terima kasih kerana sertai seminar ini.
+
+Lalu kita pergi makan dan solat zohor dan asar jamak.
+
+Lepas itu kita kembali dalam dewan.
+
+Nampak bekas artis lalu lalang depan kita tetapi kita buat-buat tidak perasan.
+
+Teringin hendak networking dengan 3 guru bisnes tadi namun ramai orang keliling mereka.
+
+Almaklum kita ini orang kampung Muo, bisnes pun kecil lagi.
+
+Kebetulan orang kampung Kodiang katanya lalu depan kita dan buat networking dengan beberapa orang.
+
+Kita pun ambil kesempatan bersalam dengan beliau selepas tengok line clear.
+
+Selepas kita bagi peluang orang lain bergambar dengan beliau.
+
+Masa itu kita dalam mood introvert, tidak ingin cakap dengan orang.
+
+Atas pentas, semasa beliau bercakap ke arah kita, kita angguk sahaja.
+
+Teringat beliau sebut tentang rupa kurma macam bentuk lipas, kita angguk sahaja.
+
+Bersambung...
--- sumber/fail/txt/seminar/limagurubisnes11.php
+++ sumber/fail/txt/seminar/limagurubisnes11.php
@@ -1,4 +1,34 @@
 #ceritaharikhamis #pelaburan
 Bismillah. Seminar di malaysia - bahagian 11.
 
-Ulasan kita tentang seminar 5 guru bisnes
+Ulasan kita tentang seminar 5 guru bisnes.
+
+Orang kita sekarang sudah berani tanya soalan, cuma tidak pandai tanya soalan pendek.
+
+Misalannya soalan pendek adalah tiga tolak satu sama dengan berapa.
+
+Orang kita tanya soalan panjang seperti boboiboy pergi kedai koko tok abah ikut jalan jauh.
+
+Naik bukit, turun bukit, dikejar anjing.
+
+Pada hal kedai koko tok abah depan rumah sahaja.
+
+Bagi kita itu satu perkembangan yang menarik.
+
+Antara soalan yang ditanyakan adalah macam mana hendak cari pekerja aka staf yang muda dan pujuk agar setia dan dengar cakap.
+
+Beberapa guru bisnes cakap, method kita terhadap orang muda kena berubah.
+
+Definasi kita bekerja keras berbeza dengan definasi orang muda bekerja keras.
+
+Mereka(guru bisnes) pun tidak tahu sejauh mana orang muda boleh dipercayai jika mereka tidak diberi peluang.
+
+Mereka cakap harus bagi peluang dan kepercayaan kepada orang muda sehingga mereka kianati kepercayaan kita.
+
+Ada yang takut tidak ada bisnes lagi di masa hadapan.
+
+Apabila mendengar beberapa guru bisnes bercerita tentang pengalaman mereka dalam bisnes, kebanyakan orang kita merasa tenang dan yakin meneruskan cabaran dalam bisnes di masa hadapan.
+
+Tengok gerai jualan di luar dewan sudah ada guna Maybank pay.
+
+Mungkin masa hadapan boleh tambah Grabpay, Tng Pay dan Booth Pay.
