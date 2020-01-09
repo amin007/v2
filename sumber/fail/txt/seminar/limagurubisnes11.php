6ffd6f7ba063624b5efbf6b2c190028ae8e31574
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-01-09 15:21:30 +0800

    tambah nota tambahan

--- sumber/fail/txt/seminar/limagurubisnes11.php
+++ sumber/fail/txt/seminar/limagurubisnes11.php
@@ -3,6 +3,8 @@ Bismillah. Seminar di malaysia - bahagian 11.
 
 Ulasan kita tentang seminar 5 guru bisnes.
 
+Nombor 1:
+
 Orang kita sekarang sudah berani tanya soalan, cuma tidak pandai tanya soalan pendek.
 
 Misalannya soalan pendek adalah tiga tolak satu sama dengan berapa.
@@ -25,9 +27,60 @@ Mereka(guru bisnes) pun tidak tahu sejauh mana orang muda boleh dipercayai jika 
 
 Mereka cakap harus bagi peluang dan kepercayaan kepada orang muda sehingga mereka kianati kepercayaan kita.
 
+Nombor 2:
+
 Ada yang takut tidak ada bisnes lagi di masa hadapan.
 
 Apabila mendengar beberapa guru bisnes bercerita tentang pengalaman mereka dalam bisnes, kebanyakan orang kita merasa tenang dan yakin meneruskan cabaran dalam bisnes di masa hadapan.
+
+Nombor 3:
+
+Menerapkan islam dalam dunia perniagaan sangat mencabar.
+
+Kena pula ada banyak mazhab dalam islam, apa yang kita kata islam akan ada orang lain menentang pendapat kita yang mengatakan bahawa itu bukan islam.
+
+Kadang-kala kita harus melihat sirah nabi dari sudut berbeza.
+
+Dalam definasi lama, nabi muhammad dianggap mengambil upah mengembala kambing.
+
+Dalam definasi baru, nabi muhammad dianggap buat servis mengembala kambing.
+
+Itu serupa dengan kes ambil upah potong rumput atau buat bisnes potong rumput.
+
+Begitu juga dengan kes teksi sapu atau buat grabcar.
+
+Aktiviti yang sama, definasi yang berbeza.
+
+Menurut sumber laman web Pejabat Mutfi Kuala Lumpur,
+https://muftiwp.gov.my/ms/artikel/irsyad-al-hadith/2689-irsyad-al-hadith-siri-ke-292-perniagaan-adalah-sumber-rezeki-terbesar
+
+Sebuah hadith yang diriwayatkan oleh Abu Sa'id RA, bahawa Nabi SAW bersabda:
+"Peniaga yang jujur dan amanah akan bersama para nabi, orang yang jujur dan orang yang syahid."
+[Riwayat al-Tirmizi (1209)][Hadith ini adalah hasan menurut Imam al-Tirmizi]
+
+Sebuah hadith yang lain pula, bahawa Nabi SAW pernah bersabda:
+"Sesungguhnya para peniaga ini akan dibangkitkan pada hari Kiamat dalam keadaan yang berdosa melainkan orang yang bertaqwa kepada Allah, berbuat kebaikan dan jujur."
+[Riwayat al-Tirmizi (1210)][Hadith ini adalah hasan sahih menurut Imam al-Tirmizi dan hadith ini disahihkan oleh Imam Ibn Hibban di dalam sahihnya]
+
+Dalam bidang programming, ada konsep mvc - model view controller.
+
+Lepas itu, kita ubah konsep mvc kepada mvc-melayu kepada penggunaan bahasa melayu dalam pengaturcaraan.
+
+Ikutkan dari situ, kita boleh kembangkan dari mvc-melayu kepada cerita karton Boboiboy Kuasa 3,5,7,9 dan sebagainya.
+
+Kita belajar istilah-istilah perniagaan dan akaun dari barat.
+
+Akan tetapi ada istilah-istilah tersebut sudah ada dalam dunia islam.
+
+Cuma jika kita ingin menambah konsep baru yang berbentuk islamik dalam perniagaan, mungkin akan ada orang lain tentang idea kita.
+
+Kita harus tabah dalam mengaplikasi dalam bisnes yang kita buat.
+
+Lepas itu kita baiki dari semasa ke semasa.
+
+Orang akan suka cari salah kita dan sebarkan.
+
+Nombor 4:
 
 Tengok gerai jualan di luar dewan sudah ada guna Maybank pay.
 
