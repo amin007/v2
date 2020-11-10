8d4ce2a96851774b02a6b2efc2a8e54f1b79fc96
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-11-10 08:32:08 +0800

    tambah tatasusunan baru

--- sumber/fail/rujukan/kod00.php
+++ sumber/fail/rujukan/kod00.php
@@ -14,6 +14,33 @@ $data['respon'] = array(
 	array('','00','00 - Lain-Lain Didiami'),
 );
 #--------------------------------------------------------------------------------------------------
+/* Kod Bahasa (LT)(2 digit)
+Kod bahasa yang utama semasa temu ramah. Sila lihat Butiran 16 di Butir-Butir Pengenalan STB/LFS-1
+*/
+$data['kod bahasa'] = array(
+	array('','10','10 - Melayu'),
+	array('','20','20 - Inggeris'),
+	array('','30','30 - Mandarin'),
+	array('','31','31 - Hokkien'),
+	array('','32','32 - Kantonis'),
+	array('','33','33 - Hakka'),
+	array('','34','34 - Teochew'),
+	array('','35','35 - Hainan'),
+	array('','36','36 - Foochow'),
+	array('','40','40 - Tamil'),
+	array('','41','41 - India lain'),
+	array('','50','50 - Iban'),
+	array('','51','51 - Bidayuh'),
+	array('','52','52 - Melanau'),
+	array('','53','53 - Murut'),
+	array('','60','60 - Kadazan'),
+	array('','61','61 - Dusun'),
+	array('','62','62 - Bajau'),
+	array('','63','63 - Rungus'),
+	array('','64','64 - Brunei'),
+	array('','70','70 - Lain-lain'),
+);
+#--------------------------------------------------------------------------------------------------
 //<tr class="table-primary"><td colspan="2">Kod AIR</td></tr>
 $data['AIR'] = array(
 	array('','01','01 - Ketua isi rumah aka KIR'),
