c1d11fd090d2f53cb0f68d3ba55e9713ba188116
Amin Ledang @ Dell <helmi007@yahoo.com>
2020-11-11 20:02:41 +0800

    simpan kod daa

---
+++ sumber/fail/txt/Takbir raya pkp.php
@@ -0,0 +1,20 @@
+Cheat code untuk pak imam.
+
+Allahu akbar Allahu akbar Allahu akbar
+Laa ilaaha illallahu wallahu akbar
+Allaahu akbar walillaahil hamd’
+
+ulang 3 kali
+
+Allahu akbar Allahu akbar Allahu akbar
+Allahu akbar kabiiraa
+Walhamdulillaahi katsiiraa
+Wasubhaanallaahi bukrataw wa asiillaa
+Laa ilaaha illallallahu walaa na’budu illaa iyyaahu mukhlishiina lahuddiin
+Walau karihal kaafiruun
+
+Laa ilaaha illallallahu wahdah, sadaqa wa’dah, wanashara ‘abdah,
+wa a’azza jundahu wahazamal ahzaaba wahdah
+
+Laa ilaaha illallaahu wallaahu akbar
+Allaahu akbar walillaahil hamd’
