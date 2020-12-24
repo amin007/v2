7c948459d30849f27573cc7d9fbf24938c030cef
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2020-12-24 12:40:02 +0800

    tambah kod negeri JPN

--- sumber/fail/rujukan/kod00.php
+++ sumber/fail/rujukan/kod00.php
@@ -1240,6 +1240,27 @@ $data['bandar'] = array(
 	array('','16 - Putrajaya','01 - Putrajaya','710 - Putrajaya'),
 );
 #--------------------------------------------------------------------------------------------------
+// https://www.jpn.gov.my/kod-negeri
+$data['KodNegeriJPN'] = array(
+	array('','Johor - 01, 21, 22, 23, 24'),
+	array('','Kedah - 02, 25, 26, 27'),
+	array('','Kelantan - 03, 28, 29'),
+	array('','Melaka - 04, 30'),
+	array('','Negeri Sembilan - 05, 31, 59'),
+	array('','Pahang - 06, 32, 33'),
+	array('','Pulau Pinang - 07, 34, 35'),
+	array('','Perak - 08, 36, 37, 38 , 39'),
+	array('','Perlis - 09, 40'),
+	array('','Selangor - 10, 41, 42, 43, 44'),
+	array('','Terengganu - 11, 45, 46'),
+	array('','Sabah - 12, 47, 48, 49'),
+	array('','Sarawak - 13, 50, 51, 52, 53'),
+	array('','Wilayah Persekutuan (Kuala Lumpur) - 14, 54, 55, 56, 57'),
+	array('','Wilayah Persekutuan (Labuan) - 15, 58'),
+	array('','Wilayah Persekutuan (Putrajaya) - 16'),
+	array('','Negeri Tidak Diketahui - 82'),
+);
+#--------------------------------------------------------------------------------------------------
 // https://www.jpn.gov.my/kod-negara/
 $data['KodNegaraJPN'] = array(
 	array('','','<a target="_blank" href="https://www.jpn.gov.my/kod-negara">https://www.jpn.gov.my/kod-negara</a> 0123456789'),
