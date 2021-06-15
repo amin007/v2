952238031196ba00c96e40ec99533b919ce875ca
Amin Ledang @Dell01 <helmi007@yahoo.com>
2021-06-15 20:11:20 +0800

    tambah contoh muka depan daa

---
+++ Aplikasi/Fail/Papar/index/mukautama.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Halaman Amin007 Ledang</title>
+<link href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" rel="stylesheet" type="text/css">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+<style type="text/css">
+html, body {
+	height: 100%;
+	background: url("https://cdn.pixabay.com/photo/2016/04/17/20/19/woman-1335487_960_720.jpg")
+	/*background: url("https://cdn.pixabay.com/photo/2015/09/19/01/03/guitar-946701_960_720.jpg")*/
+	/*background: url("https://cdn.pixabay.com/photo/2016/08/26/07/43/the-1621517_960_720.jpg")*/
+	/*background: url("https://cdn.pixabay.com/photo/2015/09/02/12/57/woman-918776_960_720.jpg")*/
+	/*background: url("https://cdn.pixabay.com/photo/2016/05/27/21/11/girl-1420629_960_720.jpg")*/
+	/*background: url("https://cdn.pixabay.com/photo/2015/05/01/10/34/woman-748211_960_720.jpg")*/
+	/*background: url("https://cdn.pixabay.com/photo/2018/07/25/15/51/hijab-3561651_960_720.jpg")*/
+	/*background: url("https://cdn.nohat.cc/thumb/f/720/4530650874904576.jpg")*/
+	/*background: url("https://cdn.pixabay.com/photo/2015/09/04/19/32/landscape-922581_960_720.png")*/
+	no-repeat center center fixed;background-size: cover;
+}
+body {
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	display: table;
+	font-weight: 100;
+}
+.kotakAtas {
+	text-align: center;
+	display: table-cell;
+	vertical-align: middle;
+}
+.kotakTengah {
+	text-align: center;
+	display: inline-block;
+}
+.well {
+	background-color: rgba(255, 255, 255, 0.7);
+}
+</style>
+</head>
+<body>
+
+<div class="kotakAtas">
+<div class="kotakTengah">
+	<ul class="nav nav-tabs well" id="myTab" role="tablist">
+	<li class="nav-item">
+		<a class="nav-link active" id="anjung-tab" data-toggle="tab" href="#anjung" role="tab" aria-controls="anjung" aria-selected="true">Anjung</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="warna-tab" data-toggle="tab" href="#warna" role="tab" aria-controls="warna" aria-selected="false">Warna</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="login-tab" data-toggle="tab" href="#login" role="tab" aria-controls="login" aria-selected="false">Login</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" id="projek-tab" data-toggle="tab" href="#projek" role="tab" aria-controls="projek" aria-selected="false">Projek</a>
+	</li>
+	</ul>
+	<div class="tab-content" id="myTabContent">
+		<div class="tab-pane fade show active" id="anjung" role="tabpanel" aria-labelledby="anjung-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br><form>
+	<div class="form-group">
+		<label for="a" class="bg-white">
+			<a href="v3/">Selamat datang ke halaman kami</a>
+		</label><br>
+		<label for="b" class="well">Macam mana kehidupan anda pada hari ini</label><br>
+		<label for="c" class="well">Semoga anda ceria sepanjang masa</label>
+	</div><!-- / class="form-group" -->
+</form>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="warna" role="tabpanel" aria-labelledby="warna-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br>
+	<div class="p-3 mb-2 bg-transparent text-dark shadow-lg rounded">
+		<a href="v3/rujukan">Rujukan Utama</a>
+	</div>
+	<div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+	<div class="p-3 mb-2 bg-secondary text-white">.bg-secondary</div>
+	<div class="p-3 mb-2 bg-success text-white">.bg-success</div>
+	<div class="p-3 mb-2 bg-danger text-white">.bg-danger</div>
+	<div class="p-3 mb-2 bg-warning text-dark">.bg-warning</div>
+	<div class="p-3 mb-2 bg-info text-white">.bg-info</div>
+	<div class="p-3 mb-2 bg-light text-dark">.bg-light</div>
+	<div class="p-3 mb-2 bg-dark text-white">.bg-dark</div>
+	<div class="p-3 mb-2 bg-white text-dark">.bg-white</div>
+	<div class="p-3 mb-2 bg-transparent text-dark">.bg-transparent</div>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="login" role="tabpanel" aria-labelledby="login-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br><form>
+	<div class="p-3 mb-2 bg-transparent text-dark">
+		<i class="fa fa-bicycle" style="font-size:50px"></i>
+	</div><!-- / class="p-3 mb-2 bg-transparent text-dark" -->
+	<div class="form-group">
+		<div class="btn btn-secondary btn-block text-white">Login dari Facebook <i class="fab fa-facebook"></i></div>
+		<div class="btn btn-secondary btn-block text-white">Login dari Github <i class="fab fa-github"></i></div>
+		<div class="btn btn-secondary btn-block text-white">Login dari Google <i class='fab fa-google'></i></div>
+		<div class="btn btn-secondary btn-block text-white">Login dari Twitter <i class="fab fa-twitter-square"></i></div>
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label class="border border-dark btn-block">Atau/Or</label>
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<label for="exampleInputEmail1">Email</label>
+		<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan Email">
+		<small id="emailHelp" class="form-text text-muted">Kita berjanji tidak akan berkongsi kasih<br>
+		eh salah berkongsi email dengan orang lain.</small>
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Rahsia Antara Kita</label>
+		<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+	</div>
+	<div class="form-group form-check">
+		<input type="checkbox" class="form-check-input" id="exampleCheck1">
+		<label class="form-check-label" for="exampleCheck1">Kenang daku dalam doamu</label>
+	</div>
+	<div class="form-group">
+		<input type="submit" class="btn btn-primary btn-block" value="Hantar">
+	</div><!-- / class="form-group" -->
+	<div class="form-group">
+		<a class="btn btn-dark btn-block text-white" href="https://www.w3schools.com/icons/">
+		Rujukan Icon <i class="fas fa-headphones"></i></a>
+	</div><!-- / class="form-group" -->
+</form>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+		<div class="tab-pane fade" id="projek" role="tabpanel" aria-labelledby="projek-tab">
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<br><form>
+	<div class="form-group">
+		<div class="bg-secondary text-white"><label class="kotakLabel" for="contohNama">
+			Belum putuskan lagi hendak letak apa di sini
+		<label></div>
+	</div><!-- / class="form-group" -->
+</form>
+<!-- ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+		</div>
+	</div><!-- / class="tab-content" id="myTabContent" -->
+</div><!-- / class="kotakTengah" -->
+</div><!-- / class="kotakAtas" -->
+
+<!-- iklan google -->
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+	(adsbygoogle = window.adsbygoogle || []).push({
+		google_ad_client: "ca-pub-2307262272744070",
+		enable_page_level_ads: true
+	});
+</script>
+<!-- iklan google -->
+
+<!-- Footer
+================================================== -->
+<nav class="navbar fixed-bottom navbar-light">
+	<span class="bg-secondary text-white">&copy; Hak Cipta Terperihara 2019 </span>
+	<span class="bg-secondary text-white">Theme Asal Bootstrap Twitter </span>
+	<span class="bg-secondary text-white">PHP version: 7.2.34</span>
+	<span class="bg-transparent text-white">Translate</span>
+	<span class="bg-secondary text-white">English</span>
+	<span class="bg-secondary text-white">Malay</span>
+</nav>
+
+<!-- khas untuk jquery dan js2 lain
+================================================== -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
