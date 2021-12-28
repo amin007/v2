3eb9092814f7040f92e477a8ba4402a702c66816
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2021-12-28 10:54:26 +0800

    tambah koding baru

--- sumber/fail/rujukan/kod2022.php
+++ sumber/fail/rujukan/kod2022.php
@@ -230,7 +230,7 @@ $data['sijil 2022'] = array(
 	. '- Tidak tertakluk kepada responden yang dicatatkan sebagai ‘Pendidikan Bukan Formal` (Kod 006) sahaja pada Ruangan 16.'),
 );
 #--------------------------------------------------------------------------------------------------
-$data['Pendidikan&Sijil 2022'] = array(
+$data['PendidikanSijil 2022'] = array(
 	array('','001','001 - Pendidikan awal kanak-kanak( kurang dari 3 tahun','020 - Tiada Sijil'),
 	array('','002','002 - Pendidikan pra-sekolah','020 - Tiada Sijil'),
 	array('','003','003 - Pendidikan tidak formal','020 - Tiada Sijil<br>'
@@ -535,3 +535,67 @@ $data['Pengajian 2022'] = array(
 	array('','99 : Lain-lain<br>999 : Lain-lain<br>','9999 - Lain-lain'),
 );
 #--------------------------------------------------------------------------------------------------
+###################################################################################################
+include 'fungsi_global.php';
+###################################################################################################
+#--------------------------------------------------------------------------------------------------
+// bina tatasusunan dari fungsi
+//$tahunJadual = kiraTahunJadual();
+//semakPembolehubah(array($tahunJadual),'tahunJadual');
+//$data['tahun'] = array(array($tahunJadual));
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
+# mula koding
+#--------------------------------------------------------------------------------------------------
+/*
+$_SERVER['PATH_INFO'] => untuk apache
+$_SERVER['REQUEST_URI'] =
+$_SERVER['PATH_TRANSLATED']
+$_SERVER['PHP_SELF']
+$_SERVER['QUERY_STRING'] => /bandar
+$_SERVER['REQUEST_SCHEME'] => https
+$_SERVER['SERVER_PORT'] => 80 atau 443
+
+$s = 'PHP_SELF';
+echo $_SERVER[$s] . '|<br>';
+$cari0 = explode('kod00.php',$_SERVER[$s]);
+$cari2 = explode('/',$cari0[1]);
+echo '<pre>'; print_r($cari2); echo '</pre>';
+*/
+#--------------------------------------------------------------------------------------------------
+# kaedah 1
+/*if (isset($_GET['cari'])):
+	$cari = $_GET['cari'];
+	//echo '<pre>$cari='; print_r($cari); echo '</pre>';
+	panggilDataTable($data,$cari);# panggil fungsi
+else:
+	panggilDataTable($data,null);# panggil fungsi
+endif;//*/
+#--------------------------------------------------------------------------------------------------
+# kaedah 2
+$s = 'REQUEST_URI';//$s = 'PHP_SELF';
+//semakPembolehubah($_SERVER['REQUEST_URI'],'REQUEST_URI');
+if (isset($_SERVER[$s])):
+	$fail = explode('kod2022.php',$_SERVER[$s]);
+	$cari = explode('/',$fail[1]);//semakPembolehubah($fail,'fail');
+	//semakPembolehubah($cari,'pilih');
+
+	if(isset($cari[1])):
+		$cariApa = bersih($cari[1]);
+		if($cariApa == 'json'):
+			$pilih = isset($cari[2]) ? $cari[2] : null;
+			$cariApa = bersih($pilih);
+			binaJson($data,$pilih);
+		else:
+			panggilDataTable($data,$cariApa);# panggil fungsi
+		endif;
+	else:
+		panggilDataTable($data,null);# panggil fungsi
+	endif;
+else:
+	panggilDataTable($data,null);# panggil fungsi
+endif;//*/
+	//versiphp();
+#--------------------------------------------------------------------------------------------------
+# tamat koding
+###################################################################################################
