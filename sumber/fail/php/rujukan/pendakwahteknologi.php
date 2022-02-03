b5485aeac34c137a9761e7e35bf340da659d852b
Amin Dell Precision 3510 <helmi007@yahoo.com>
2022-02-03 23:28:39 +0800

    tambah panduan untuk filem percuma

---
+++ sumber/fail/php/rujukan/pendakwahteknologi.php
@@ -0,0 +1,18 @@
+<?php
+/*
+https://youtu.be/447LIE0w3JA
+
+Senarai Platform Yang Ditunjukkan:
+1. https://www.peacocktv.com/
+2. https://pluto.tv/
+3. https://therokuchannel.roku.com/
+4. https://tubitv.com/
+5. https://www.crackle.com/
+6. https://www.vudu.com/
+7. https://www.imdb.com/tv/
+8. https://www.xumo.tv/
+
+Servis VPN TunnelBear
+https://cutt.ly/TunnelBear
+
+//*/
