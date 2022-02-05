58ea1e1af92dd06476b52832828d684dfc2aaf49
Amin Dell Precision 3510 <helmi007@yahoo.com>
2022-02-05 18:45:54 +0800

    buang slash

--- Aplikasi/Kelas/Kitab/DB/DB_Pdo.php
+++ Aplikasi/Kelas/Kitab/DB/DB_Pdo.php
@@ -8,7 +8,8 @@ class DB_Pdo extends \PDO
 	{
 		try
 		{
-			parent::__construct($DB_TYPE . ':host=' . $DB_HOST . ';dbname=' . $DB_NAME,\$DB_USER, $DB_PASS);
+			parent::__construct($DB_TYPE . ':host=' . $DB_HOST . ';dbname=' . $DB_NAME,
+			$DB_USER, $DB_PASS);
 			//parent::setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTIONS);
 		}
 		catch (PDOException $e) 
