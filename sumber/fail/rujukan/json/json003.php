747d1de999430bd514eaf4e6c4209d230f9ccc5d
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-02-06 13:46:14 +0800

    semak data json

---
+++ sumber/fail/rujukan/json/json003.php
@@ -0,0 +1,38 @@
+<?php
+#--------------------------------------------------------------------------------------------------
+	function semakPembolehubah($senarai,$jadual='entahlah',$p='0')
+	{
+		# semak $senarai adalah array atau tidak
+		$semak = is_array($senarai) ? 'array' : 'bukan';
+		if($semak == 'array'):
+			echo '<pre>$' . $jadual . '=><br>';
+			if($p == '0') print_r($senarai);
+			if($p == '1') var_export($senarai);
+			echo '</pre>' . "\n";
+		else:
+			echo tagVar($senarai,$jadual,2);
+		endif;
+		//$this->semakPembolehubah($ujian,'ujian',0);
+		#http://php.net/manual/en/function.var-export.php
+		#http://php.net/manual/en/function.print-r.php
+	}
+#--------------------------------------------------------------------------------------------------
+	function tagVar($senarai,$jadual,$pilih=2)
+	{
+		# set pembolehubah utama
+		$p1 = 'pre';#https://www.w3schools.com/tags/tag_var.asp
+		$p2 = 'kbd';
+		$p3 = 'code';
+		$p4 = 'samp';
+		# setkan tatasusunan
+		$p[1] = "<$p1>\$$jadual = $senarai</$p1><br>\n";
+		$p[2] = "<$p2>\$$jadual = $senarai</$p2><br>\n";
+		$p[3] = "<$p3>\$$jadual = $senarai</$p3><br>\n";
+		$p[4] = "<$p4>\$$jadual = $senarai</$p4><br>\n";
+		#
+		return $p[$pilih];
+	}
+#--------------------------------------------------------------------------------------------------
+$string = file_get_contents('../kod2022/masco2020_newss.json');
+$data = json_decode($string, true);
+semakPembolehubah($data,'data json');
