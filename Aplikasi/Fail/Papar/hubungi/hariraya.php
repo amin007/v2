b29a9f96bcc2fd4303928b5005565b3d83ec4534
Amin Dell Precision 3510 <helmi007@yahoo.com>
2022-05-02 16:08:28 +0800

    tambah koding baru

--- Aplikasi/Fail/Papar/hubungi/hariraya.php
+++ Aplikasi/Fail/Papar/hubungi/hariraya.php
@@ -4,21 +4,27 @@
 <hr>
 <h1 class="text-center">&nbsp;<span class="bg-secondary butangBulat">&nbsp;<?php echo $this->Tajuk_Muka_Surat; ?>&nbsp;</span></h1>
 <!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="container">
 <div class="row">
-	<div class="col-lg-6">
-		<img src="./sumber/gambar/hariraya/clean-eid-mubarak-mosque-banner-design/18445.jpg">
+	<div class="col-lg-12">
+		<img src="<?php echo URL ?>sumber/gambar/hariraya/clean-eid-mubarak-mosque-banner-design/18445.jpg"
+		height="100%" width="100%"><hr>
 		<a href="https://www.freepik.com/vectors/happy-ramadan">Happy ramadan vector created by starline - www.freepik.com</a>
 	</div><!-- / class="col-lg-6" -->
 </div><!-- / class="row" -->
+</div><!-- / class="container" -->
 <!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
+<div class="container">
 <div class="row">
-	<div class="col-lg-6">
-		<img src="./sumber/gambar/hariraya/clean-eid-mubarak-mosque-banner-design/18779.jpg">
+	<div class="col-lg-12">
+		<img src="<?php echo URL ?>sumber/gambar/hariraya/mosque-shape-clouds-eid-mubarak-background/18799.jpg"
+		height="100%" width="100%"><hr>
 		<a href="https://www.freepik.com/vectors/happy-ramadan">Happy ramadan vector created by starline - www.freepik.com</a>
 	</div><!-- / class="col-lg-6" -->
 </div><!-- / class="row" -->
+</div><!-- / class="container" -->
 <!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
-<div class="row"><div class="col-lg-6">
+<div class="row"><div class="col-lg-12">
 </div><!-- / class="col-lg-6" -->
 </div><!-- / class="row" -->
 <!-- ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// -->
