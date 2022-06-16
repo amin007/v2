a501c4873083b9680b883d7d4c7826bbc6fe53d6
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-06-16 08:52:44 +0800

    buat fungsi baru

--- sumber/fail/rujukan/fungsi_global.php
+++ sumber/fail/rujukan/fungsi_global.php
@@ -515,6 +515,60 @@ endif;//*/
 #--------------------------------------------------------------------------------------------------
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
+if ( ! function_exists('binaButangV00')):
+	function binaButangV00($senarai)
+	{
+		$output = "\r\t";
+		$output .= '<a class="btn btn-primary" href="../">Kembali'
+		. '<i class="fa fa-binoculars"></i></a>'
+		. "\n\t" . '<a class="btn btn-success rounded-pill"'
+		. ' target="_blank" href="../rujukan/utama/msic-cari.html">MSIC</a>'
+		. "\n\t" . '<a class="btn btn-success rounded-pill"'
+		. ' target="_blank" href="../rujukan/utama/masco-cari.html">MASCO2018</a>'
+		. "\n\t" . '<a class="btn btn-success rounded-pill"'
+		. ' target="_blank" href="../rujukan/utama/masco2020-cari.html">MASCO2020</a>'
+		. "\n\t" . '<a class="btn btn-info rounded-pill"'
+		. ' target="_blank" href="../rujukan/utama/institut-cari.html">Institut</a>'
+		. "\n\t" . '<a class="btn btn-info rounded-pill"'
+		. ' target="_blank" href="../rujukan/utama/negara-cari.html">Negara</a>'
+		. "\n\t" . '<a class="btn btn-warning rounded-pill"'
+		. ' target="_blank" href="./kod00.php">kod-lama</a>'
+		. "\n\t" . '<a class="btn btn-warning rounded-pill"'
+		. ' target="_blank" href="./kod2022.php">kod2022</a>'
+		. "\n\t" . '<a class="btn btn-warning rounded-pill"'
+		. ' target="_blank" href="./kod2022_v01.php">kod2022_v01</a>'
+		. "\n\t" . '<a class="btn btn-outline-secondary rounded-pill"'
+		. ' href="' . URL . '?/tahun">Tahun</a>';
+		/*$koleksi = array(
+			array('a'=>'primary','b'=>'../','c'=>'Kembalilah<i class="fa fa-binoculars"></i>'),
+			array('a'=>'success','b'=>'../rujukan/utama/msic-cari.html','c'=>'MSIC'),
+			array('a'=>'success','b'=>'../rujukan/utama/masco-cari.html','c'=>'MASCO2018'),
+			array('a'=>'success','b'=>'../rujukan/utama/masco2020-cari.html','c'=>'MASCO2020'),
+			array('a'=>'info','b'=>'../rujukan/utama/institut-cari.html','c'=>'Institut'),
+			array('a'=>'info','b'=>'../rujukan/utama/negara-cari.html','c'=>'Negara'),
+			array('a'=>'warning','b'=>'./kod00.php','c'=>'kod-lama'),
+			array('a'=>'warning','b'=>'./kod2022.php','c'=>'kod2022'),
+			array('a'=>'warning','b'=>'./kod2022_v01.php','c'=>'kod2022_v01'),
+			array('a'=>'outline-secondary','b'=>URL . '?/tahun','c'=>'Tahun')
+		);
+		foreach($koleksi as $masa => $kini):
+			$output .= "\n\t" . '<a class="btn btn-' . $kini['a'] . ' rounded-pill"'
+			. ' href="' . URL . '/' . $kini['b'] . '">'
+			. ucfirst($kini['c']) . '</a>';
+		endforeach;//*/
+		foreach($senarai as $jadual => $row):
+			if($jadual != 'tahun')
+			$output .= "\n\t" . '<a class="btn btn-outline-secondary rounded-pill"'
+			. ' href="' . URL . '?/' .$jadual. '">'
+			. ucfirst($jadual) . '</a>';
+		endforeach;
+		$output .= "\n\t<hr>";
+
+		echo $output;
+		#
+	}
+endif;//*/
+#--------------------------------------------------------------------------------------------------
 if ( ! function_exists('binaButang')):
 	function binaButang($senarai)
 	{
