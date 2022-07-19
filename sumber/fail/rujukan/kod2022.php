3b9135c24e499db96b33da392712bb74a730947a
amin007 <helmi007@yahoo.com>
2022-07-19 20:30:23 +0800

    tambah <br>

--- sumber/fail/rujukan/kod2022.php
+++ sumber/fail/rujukan/kod2022.php
@@ -694,14 +694,14 @@ $data['Pendidikan 2022'] = array(
 	. '- Peringkat ini dikhaskan kepada program pendidikan/penyelidikan selepas seseorang itu telah memperolehi ijazah falsafah kedoktoran bertujuan untuk memperoleh kemahiran profesional yang diperlukan dalam kemajuan kerjaya.<br>'
 	. '- Pelajar yang mengikuti program ini akan memegang jawatan postdoctoral fellow bagi tempoh 6 bulan hingga 5 tahun yang dikhususkan untuk menjalankan penyelidikan. Program ini juga telah ditawarkan kepada mereka yang memfokuskan kerjaya dalam bidang pendidikan yang dikenali sebagai "teaching post-docs".'
 	. ''),
-	array('','1xx','Pendidikan rendah:','(umur pelajar: 6-12 tahun); (pengajian terkumpul: 1-6 tahun)'
+	array('','1xx','Pendidikan rendah:','(umur pelajar: 6-12 tahun); (pengajian terkumpul: 1-6 tahun)<br>'
 	. '- Direkabentuk untuk menyediakan pendidikan umum bertujuan melengkapkan murid-murid dengan kemahiran asas membaca, menulis dan mengira (3M) serta mewujudkan individu yang seimbang.<br>'
 	. '- Murid-murid akan mengambil ujian penilaian di peringkat akhir pendidikan rendah, sebelum menyambung pelajaran ke peringkat menengah rendah.<br>'
 	. '- Tempoh pengajian enam tahun tetapi boleh tamat diikuti dalam tempoh lima hingga tujuh tahun.<br>'
 	. '- Kelayakan masuk mengikut peraturan yang ditetapkan oleh kerajaan atau sekolah rendah berkenaan.<br>'
 	. '- Permulaan aktiviti pembacaan sahaja bukan kriteria untuk klasifikasi program pendidikan ini.<br>'
 	. ''),
-	array('','2xx','Pendidikan Menengah Rendah','(umur pelajar: 12-15 tahun); (pengajian terkumpul: 7-10 tahun)'
+	array('','2xx','Pendidikan Menengah Rendah','(umur pelajar: 12-15 tahun); (pengajian terkumpul: 7-10 tahun)<br>'
 	. '- Meneruskan program pendidikan asas yang dijalankan di peringkat rendah.<br>'
 	. '- Disediakan dengan memberi lebih masa kepada murid-murid untuk menguasai dan melengkapkan diri dengan pendidikan umum yang bersepadu.<br>'
 	. '- Mesti mengikut masa di mana ianya dirancang berorientasikan subjek menggunakan guru yang berkelayakan khusus untuk bidang/subjek yang diajar.<br>'
