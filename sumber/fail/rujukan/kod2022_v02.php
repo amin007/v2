275915bd6e9f646a3f13751253745e96cf11f907
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-07-26 10:44:13 +0800

    tambah contoh semakDNS

--- sumber/fail/rujukan/kod2022_v02.php
+++ sumber/fail/rujukan/kod2022_v02.php
@@ -3,6 +3,33 @@
 include 'fungsi_global.php';
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
+if ( ! function_exists('getClientIp')):
+	function getClientIp($pilih = 1)
+	{# Function to get the client IP address
+		$ipaddress = '';
+		if (isset($_SERVER['HTTP_CLIENT_IP']))
+			$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+		else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+			$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		else if(isset($_SERVER['HTTP_X_FORWARDED']))
+			$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+		else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+			$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+		else if(isset($_SERVER['HTTP_FORWARDED']))
+			$ipaddress = $_SERVER['HTTP_FORWARDED'];
+		else if(isset($_SERVER['REMOTE_ADDR']))
+			$ipaddress = $_SERVER['REMOTE_ADDR'];
+		else
+			$ipaddress = 'UNKNOWN';
+		#
+		$ip = ($pilih == 1) ? $ipaddress . "<br>\n" : $ipaddress;
+		#
+		return $ip;
+	}
+endif;//*/
+#--------------------------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------------
+###################################################################################################
 $arr = get_defined_functions();
 $kira = count($arr['internal']);
 $kira2 = count($arr['user']);
@@ -43,4 +70,24 @@ $arr['user']=Array
     [26] => binabutang
     [27] => panggildatatable
 )*/
+#--------------------------------------------------------------------------------------------------
+# 2. isytiharkan zon masa => Asia/Kuala Lumpur
+date_default_timezone_set('Asia/Kuala_Lumpur');
+# 3. semak ip dan dns
+$tajuk[] = date("h:i:s",$_SERVER['REQUEST_TIME']);
+$tajuk[] = 'getHostName = ' . getHostName();
+$tajuk[] = 'getHostByName = ' . getHostByName(getHostName());
+$tajuk[] = 'getClientIp = ' . getClientIp(0);
+$tajuk[] = '-------------------------------------------------';
+//https://www.meridianoutpost.com/resources/articles/command-line/ipconfig.php
+exec("ipconfig /all", $out, $res);
+# 4. trim data dalam tatasusunan
+$tajuk[] = trim($out[3]);
+list($tajuk[],$tajuk[]) = explode(': ',trim($out[26]));
+$tajuk[] = trim($out[27]);
+echo "<hr><pre>out = \n"; print_r($tajuk);
+# 5. papar tatasusunan yang bersih
+exec("ipconfig /displaydns", $out3, $res);
+echo "<hr>out3 = count(" . count($out3) . ")<hr>";
+semakPembolehubah($out3,'out3',0);
 #--------------------------------------------------------------------------------------------------
