8cd31ef5269189474c879f9f15ba09136933d49f
Amin Dell Precision 3510 <helmi007@yahoo.com>
2022-07-30 11:26:03 +0800

    tambah kbd

--- index.php
+++ index.php
@@ -29,11 +29,11 @@ spl_autoload_register(function ($namaClass)
 	# buat pecahan tatasusunan $namaClass
 	$class = explode('\\', $namaClass); //print_r($class) . '<br>';
 	# semak kewujudan class
-	//echo '<hr>nama class:' . $class[count($class)-1] . ' | ';
+	//echo '<hr><kbd>nama class:' . $class[count($class)-1] . ' | </kbd>';
 	$cariFail = GetMatchingFiles(GetContents('Aplikasi/Kelas'),$class[count($class)-1] . '.php');
 	# jika fail wujud, masukkan
 	foreach($cariFail as $kitabApa)
-	{	//echo '$kitabApa->' . $kitabApa . '<br>';
+	{	//echo '<kbd>$kitabApa->' . $kitabApa . '</kbd><br>';
 		if (file_exists($kitabApa)) require $kitabApa;
 		//else echo 'tidak jumpa daa<br>';
 	}//*/
