3801bc52730d4ea3860b4821997337677a2ddcc3
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-08-01 10:21:51 +0800

    tukar 114 kepada 144

--- sumber/fail/rujukan/kod2022.php
+++ sumber/fail/rujukan/kod2022.php
@@ -966,7 +966,7 @@ $data['PendidikanSijil 2022'] = array(
 	. '356 - Sijil drpd universiti @ yg setaraf<br>'
 	. ''),
 	array('','404','404 - Program sijil perguruan/kejururawatan/ kesihatan bersekutu',
-	'357 - Sijil perguruan(IP=114)(FS=0114)/kejururawatan/kesihatan bersekutu'),
+	'357 - Sijil perguruan(IP=144)(FS=0114)/kejururawatan/kesihatan bersekutu'),
 	array('','501','501 - Program diploma kemahiran khusus & teknikal',
 	'551 - Diploma dlm kemahiran khusus & teknikal'),
 	array('','502','502 - Program diploma lanjutan/ Higher National Diploma'
@@ -977,7 +977,7 @@ $data['PendidikanSijil 2022'] = array(
 	. '554 - Diploma drpd politeknik(IP=154) @ yg setaraf<br>'
 	. '555 - Diploma drpd universiti @ yg setaraf'),
 	array('','504','504 - Program diploma perguruan/ kejururawatan/ kesihatan bersekutu',
-	'556 - Diploma perguruan(IP=114)(FS=0114)/kejururawatan/kesihatan bersekutu'),
+	'556 - Diploma perguruan(IP=144)(FS=0114)/kejururawatan/kesihatan bersekutu'),
 	array('','601','601 - Program ijazah sarjana muda/ diploma lanjutan',
 	'665 - Ijazah Sarjana Muda/Diploma lanjutan'),
 	array('','701','701 - Program lepasan ijazah',
