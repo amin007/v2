946df792aa31e3485b8e85fa1698886bcc023b70
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-08-29 10:12:35 +0800

    tambah fungsi baru

--- sumber/fail/rujukan/kod2022_v03.php
+++ sumber/fail/rujukan/kod2022_v03.php
@@ -3,6 +3,48 @@
 include 'fungsi_global.php';
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
+function get_defined_functions_in_file($file)
+{
+	$source = file_get_contents($file);
+	$tokens = token_get_all($source);
+	$functions = array();
+	$nextStringIsFunc = false;
+	$inClass = false;
+	$bracesCount = 0;
+
+	foreach($tokens as $token){
+		switch($token[0]) {
+			case T_CLASS:
+				$inClass = true;
+				break;
+			case T_FUNCTION:
+				if(!$inClass) $nextStringIsFunc = true;
+				break;
+			case T_STRING:
+				if($nextStringIsFunc) {
+					$nextStringIsFunc = false;
+					$functions[] = $token[1];
+				}
+				break;
+			# Anonymous functions
+			case '(':
+			case ';':
+				$nextStringIsFunc = false;
+				break;
+			# Exclude Classes
+			case '{':
+				if($inClass) $bracesCount++;
+				break;
+			case '}':
+				if($inClass) {
+					$bracesCount--;
+					if($bracesCount === 0) $inClass = false;
+				}
+				break;
+			}
+	}
+	return $functions;
+}
 #--------------------------------------------------------------------------------------------------
 ###################################################################################################
 #--------------------------------------------------------------------------------------------------
@@ -72,7 +114,6 @@ foreach ($functions['user'] as $func)
 //print_r($functions_list);
 semakPembolehubah($functions_list,'functions_list',0);
 #--------------------------------------------------------------------------------------------------
-
 # 2. isytiharkan zon masa => Asia/Kuala Lumpur
 date_default_timezone_set('Asia/Kuala_Lumpur');
 # 3. semak ip dan dns
