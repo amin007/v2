1caf964d7bbfc86900bd40e353ee9f7e2b5f1aa9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-09-20 13:48:20 +0800

    tambah fungsi semakPembolehubah

--- Aplikasi/Fail/Papar/rujukan/index.php
+++ Aplikasi/Fail/Papar/rujukan/index.php
@@ -1,5 +1,42 @@
 <?php
 #--------------------------------------------------------------------------------------------------
+if ( ! function_exists('semakPembolehubahA')):
+	function semakPembolehubahA($senarai,$jadual='entahlah',$p=2)
+	{
+		# semak $senarai adalah array atau tidak
+		$semak = is_array($senarai) ? 'array' : 'bukan';
+		if($semak == 'array'):
+			echo '<pre>$' . $jadual . '=><br>';
+			if($p == '0') print_r($senarai);
+			if($p == '1') var_export($senarai);
+			echo '</pre>' . "\n";
+		else:
+			echo tagVar($senarai,$jadual,$p);
+		endif;
+		//$this->semakPembolehubah($ujian,'ujian',0);
+		#http://php.net/manual/en/function.var-export.php
+		#http://php.net/manual/en/function.print-r.php
+	}
+endif;//*/
+#--------------------------------------------------------------------------------------------------
+if ( ! function_exists('tagVar')):
+	function tagVar($senarai,$jadual,$pilih)
+	{
+		# set pembolehubah utama
+		$p1 = 'pre';#https://www.w3schools.com/tags/tag_var.asp
+		$p2 = 'kbd';
+		$p3 = 'code';
+		$p4 = 'samp';
+		# setkan tatasusunan
+		$p[1] = "<$p1>\$$jadual = $senarai</$p1><br>\n";
+		$p[2] = "<$p2>\$$jadual = $senarai</$p2><br>\n";
+		$p[3] = "<$p3>\$$jadual = $senarai</$p3><br>\n";
+		$p[4] = "<$p4>\$$jadual = $senarai</$p4><br>\n";
+		#
+		return $p[$pilih];
+	}
+endif;//*/
+#--------------------------------------------------------------------------------------------------
 if ( ! function_exists('binaSatuJadual')):
 	function binaSatuJadual($senarai,$pilih)
 	{
@@ -71,6 +108,7 @@ endif;//*/
 <h1> Tajuk daa </h1>
 <div style="background:#ffffff">
 <?php
+//semakPembolehubahA($this->senarai,'senarai');
 binaSatuJadual($this->senarai,$this->jadual);
 ?>
 </div>
