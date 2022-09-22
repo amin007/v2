03b0043f36c0fae69c9c640dd57f69d226d4d0aa
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-09-22 08:01:21 +0800

    tukar \r kepada \n

--- sumber/fail/rujukan/fungsi_global.php
+++ sumber/fail/rujukan/fungsi_global.php
@@ -689,7 +689,7 @@ if ( ! function_exists('panggilDataTable02')):
 		$tableID = 'myTable';
 		$tableClass = 'table table-striped table-bordered';
 		$tajuk = $tajuk[$pilih]; //semakPembolehubah($tajuk[$pilih],'tajuk',2);
-		echo "\r\t" . '<h2 class="' . $btn . '" >Kod ' . ucfirst($pilih) . '</h2>'
+		echo "\n\t" . '<h2 class="' . $btn . '" >Kod ' . ucfirst($pilih) . '</h2>'
 		. "\n" . '<table id="' . $tableID . '" class="' . $tableClass . '" style="width:100%">'
 		. "\n<thead><tr>$tajuk</tr></thead>\n<tfoot><tr>$tajuk</tr></tfoot>\n"
 		. "</table>\n";
