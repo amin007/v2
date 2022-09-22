bada7f94b99a2b27f2e0351602ffa191838b6364
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-09-22 16:23:21 +0800

    tukar cara koding

--- sumber/fail/rujukan/fungsi_global.php
+++ sumber/fail/rujukan/fungsi_global.php
@@ -771,13 +771,13 @@ if ( ! function_exists('panggilDataTable')):
 	{
 		//define ('URL', dirname('http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']));
 		define ('URL', $_SERVER['SCRIPT_NAME']);
-		include 'atas-set-001.php';//include 'diatas-001.php';
+		list($urlcss,$urljs) = linkCssJs();
 		diatas($pilih, $urlcss);
 		#------------------------------------------------------------------------------------------
 		binaButang($data);//versiphp();
 		binaSatuJadual($data,$pilih);
 		#------------------------------------------------------------------------------------------
-		include 'dibawah.php';
+		dibawah($pilih,$urljs);
 		echo "<script>\n";
 		jqueryExtendA();
 		jqueryExtendB();
