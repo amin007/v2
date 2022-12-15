26a109ac78d10277fec497048324796a15541432
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2022-12-15 12:54:05 +0800

    contoh koding keluarga 69 katanya

---
+++ Aplikasi/Fail/Papar/pautan/latihan001.php
@@ -0,0 +1,75 @@
+<style>
+/* kod ke satu */
+.text-vertical{
+	writing-mode:vertical-rl;
+	transform: scale(-1,-1);
+}
+.text-vertical001{
+	height: 50px;
+	overflow: hidden;
+	text-overflow: clip;
+	writing-mode: vertical-rl;
+	transform: rotate(180deg);
+}
+/* kod ke dua */
+.rotate{
+	writing-mode: tb-rl;
+	transform: rotate(180deg);
+}
+.h-500{ height:200px }
+.w-30{ width:10px; }
+</style>
+<!--  mula - borang ke satu ------------------------------------------------------------------- -->
+<div class="form-group row bg-white">
+	<div class="col-1"></div>
+	<div class="col-sm-6">
+		<h2>Perkataan Senget Kelapa eh salah Kepala daa</h2>
+		<table class="table table-bordered bg-white">
+		<tr><th>Bentuk</th>
+			<th class="text-vertical001">Nombor Satu Daa</th>
+			<th class="text-vertical">Nombor Dua Daa</th>
+			<th class="text-vertical">Nombor Tiga Daa</th>
+			<th class="text-vertical">Nombor Empat Daa</th>
+			<th class="text-vertical">Nombor Lima Daa</th>
+			<th class="text-vertical">Nombor Enam Daa</th>
+			<th class="text-vertical">Nombor Tujuh Daa</th>
+			<th class="text-vertical">Nombor Lapan Daa</th>
+			<th class="text-vertical">Nombor Sembilan Daa</th>
+			<th class="text-vertical">Nombor Sepuluh Daa</th>
+		</tr>
+		<tr>
+		</tr>
+			<td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>
+			<td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td>
+		</table>
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group row" -->
+<!-- tamat - borang ke satu ------------------------------------------------------------------- -->
+<!--  mula - borang ke dua -------------------------------------------------------------------- -->
+<h2><a target="_blank" href="https://codepen.io/rusdyahmad/pen/BaVeZBP">
+Keluarga 69: Huruf terbalik ke bawah</a></h2>
+
+<table class="table table-bordered bg-white">
+	<thead class="thead-light">
+	<tr>
+		<th scope="col">Item</th>
+		<th scope="col" colspan="6" class="text-center">Syarikat</th>
+		<th scope="col">Jumlah</th>
+	</tr>
+	</thead>
+	<tbody>
+	<tr>
+		<td class="h-500">content</td>
+		<td class="w-30 rotate">Fueling Station Enterpress</td>
+		<td class="w-30 rotate">Stesen Kemajuan Enterpress</td>
+		<td class="w-30 rotate">Stesen Dua</td>
+		<td class="w-30 rotate">Stesen Tiga</td>
+		<td class="w-30 rotate">Stesen Empat</td>
+		<td class="w-30 rotate">Stesen Lima</td>
+		<td class="h-500">content</td>
+	</tr>
+	</tbody>
+</table>
+<!-- tamat - borang ke dua -------------------------------------------------------------------- -->
+<!--  mula - borang ke tiga ------------------------------------------------------------------- -->
+<!-- tamat - borang ke tiga ------------------------------------------------------------------- -->
