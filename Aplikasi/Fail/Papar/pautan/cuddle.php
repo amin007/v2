c41564ef7f8dd5e64f13e780b7c36786e9d79ad1
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-01-26 10:53:10 +0800

    tambah fail baru

---
+++ Aplikasi/Fail/Papar/pautan/cuddle.php
@@ -0,0 +1,192 @@
+<!-- mula - borang utama ---------------------------------------------------------------------------------------------- -->
+<form method="POST" action="" class="form-horizontal bg-light text-dark">
+<!-- mula - input tengah ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<h1 class="display-1 text-center"><?php echo $this->Tajuk_Muka_Surat ?></h1>
+		<p class="text-center">I am lonely and cold dan in need of a cudle buddy so fill this out</p>
+	</div>
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<label for="" class="form-label font-weight-bold">Name</label>
+		<input type="text" class="form-control">
+	</div>
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-md-1">
+		<label for="" class="form-label font-weight-bold">Age</label>
+		<input type="text" class="form-control">
+	</div><!-- / class="col-md-*" -->
+	<div class="col-md-3">
+		<label for="" class="form-label font-weight-bold">Gender</label>
+		<input type="text" class="form-control">
+	</div><!-- / class="col-md-*" -->
+	<div class="col-md-4">
+		<label for="" class="form-label font-weight-bold">Phone Number</label>
+		<input type="text" class="form-control">
+	</div><!-- / class="col-md-*" -->
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<label for="" class="form-label font-weight-bold">Address</label>
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+	</div>
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-4">
+		<label for="" class="form-label font-weight-bold">Top 5 Movies</label>
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+	</div>
+	<div class="col-4">
+		<label for="" class="form-label font-weight-bold">5 Random Facts About Youself</label>
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+		<input type="text" class="form-control">
+	</div>
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<label for="" class="form-label font-weight-bold">
+		Do you mind if i give you a cute name</label>
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">Yes</label>
+		</div><!-- / ="form-check form-check-inline" -->
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">No</label>
+		</div><!-- / ="form-check form-check-inline" -->
+	</div><!-- / class="col-8" -->
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<label for="" class="form-label font-weight-bold">Can we build a fort</label>
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">Yes</label>
+		</div><!-- / ="form-check form-check-inline" -->
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">No</label>
+		</div><!-- / ="form-check form-check-inline" -->
+	</div><!-- / class="col-8" -->
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<label for="" class="form-label font-weight-bold">
+		Will there be random junk food and food fights</label>
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">Yes</label>
+		</div><!-- / ="form-check form-check-inline" -->
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">No</label>
+		</div><!-- / ="form-check form-check-inline" -->
+	</div><!-- / class="col-8" -->
+</div><!-- / class="form-group row" -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-8">
+		<label for="" class="form-label font-weight-bold">Do you mind kiss</label>
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">Yes</label>
+		</div><!-- / ="form-check form-check-inline" -->
+		<div class="form-check form-check-inline">
+			<input class="form-check-input" type="radio" name="cuteName">
+			<label class="form-check-label" for="">No</label>
+		</div><!-- / ="form-check form-check-inline" -->
+	</div><!-- / class="col-8" -->
+</div><!-- / class="form-group row" -->
+<hr>
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-4">
+		<label for="" class="form-label font-weight-bold">Include one or all</label>
+		<div class="input-group input-group-lg">
+			<span class="input-group-text">Instagram&nbsp;
+			<i class="fab fa-instagram fa-2x"></i></span>
+			<input type="text" class="form-control">
+		</div><!-- / class="input-group" -->
+		<div class="input-group input-group-lg">
+			<span class="input-group-text">Twitter&nbsp;
+			<i class="fab fa-twitter-square fa-2x"></i></span>
+			<input type="text" class="form-control">
+		</div><!-- / class="input-group" -->
+	</div><!-- / class="col-4" -->
+	<div class="col-4">
+		<div class="input-group input-group-lg">
+			<span class="input-group-text">Tumblr&nbsp;
+			<i class="fab fa-tumblr-square fa-2x"></i></span>
+			<input type="text" class="form-control">
+		</div><!-- / class="input-group" -->
+		<div class="input-group input-group-lg">
+			<span class="input-group-text">Snapchat&nbsp;
+			<i class="fab fa-snapchat-square fa-2x"></i></span>
+			<input type="text" class="form-control">
+		</div><!-- / class="input-group" -->
+		<div class="input-group input-group-lg">
+			<span class="input-group-text">TikTok&nbsp;
+			<i class="fab fa-tiktok fa-2x"></i></span>
+			<input type="text" class="form-control">
+		</div><!-- / class="input-group" -->
+	</div><!-- / class="col-4" -->
+</div><!-- / class="form-group row" -->
+<hr>
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-4">
+		<p>This is just a bunch of free space i'm going to type
+		<br>random things in because i need to and it looks
+		<br>more profesiona filled in but i hope to cuddle with
+		<br>you soon and we will watch bunches of movies and
+		<br>nap and take pictures and do cute things together.
+		</p>
+		<label for="" class="form-label font-weight-bold">Signature</label>
+		<input type="text" class="form-control">
+	</div><!-- / class="col-4" -->
+	<div class="col-4">
+		<div class="input-group mb-3">
+			<input type="file" class="form-control" id="inputGroupFile02">
+			<label class="input-group-text" for="inputGroupFile02">Upload Photo</label>
+		</div>
+		<div class="text-center">
+			<svg class="bd-placeholder-img rounded" width="300" height="150" xmlns="http://www.w3.org/2000/svg"
+			role="img" aria-label="Placeholder: 200x200" preserveAspectRatio="xMidYMid slice" focusable="false">
+			<title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="17%" y="50%"
+			fill="#dee2e6" dy=".3em">Place Photo of
+			Yourself here</text></svg>
+		</div>
+	</div><!-- / class="col-4" -->
+</div><!-- / class="form-group row" -->
+<!-- xxxx - input tengah ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+<div class="form-group row">
+	<div class="col-1"></div>
+	<div class="col-sm-8 input-group-text">
+		<div class="input-group input-group-lg">
+			<input type="hidden" name="jadual" value="@jadual">
+			<input type="submit" name="Simpan" value="Send" class="btn btn-primary btn-large">
+			<input type="reset" name="Reset" value="Reset" class="btn btn-secondary btn-large">
+		</div>
+	</div>
+</div><!-- / class="form-group row" -->
+<!-- tamat - input tengah  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+</form>
+<!-- tamat - borang utama ---------------------------------------------------------------------------------------------- -->
