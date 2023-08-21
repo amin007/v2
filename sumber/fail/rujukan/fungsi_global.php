dc10ec42c5df29d51e1ac2968acdfb618f0467e0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2023-08-21 15:49:14 +0800

    buat fungsi baru laa

--- sumber/fail/rujukan/fungsi_global.php
+++ sumber/fail/rujukan/fungsi_global.php
@@ -770,6 +770,32 @@ END;
 	}
 endif;//*/
 #--------------------------------------------------------------------------------------------------
+if ( ! function_exists('jsPanggilFailJsonV02')):
+	function jsPanggilFailJsonV02($failJson)
+	{
+		$papar = jsBuatLimitPage($pilih=1);
+		print <<<END
+	var t = $('#myTable').DataTable({
+	"ajax": "$failJson",
+	searchHighlight: true,
+	"columnDefs": [{
+		"searchable": false,
+		"orderable": false,
+		"targets": 0
+	}],
+	$papar
+	"order": [[ 1, 'asc' ]]
+    });
+/* ***************************************************************************************** */
+	t.on( 'order.dt search.dt', function (){
+		t.column(0, {search:'applied', order:'applied'}).nodes().
+		each( function (cell, i) {cell.innerHTML = i+1;});
+    }).draw();
+/* ***************************************************************************************** */
+
+END;
+	}
+endif;//*/
 #--------------------------------------------------------------------------------------------------
 # jawapan kenapa data php yang tukar json, datatables tak boleh baca
 #https://stackoverflow.com/questions/33582203/datatables-warning-table-usertable-invalid-json-response
