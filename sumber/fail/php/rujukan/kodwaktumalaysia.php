15eb22955ffae01e52a8dcdd35c171f4988a09aa
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2024-03-16 14:12:16 +0800

    refresh setiap 2 saat katanya

--- sumber/fail/php/rujukan/kodwaktumalaysia.php
+++ sumber/fail/php/rujukan/kodwaktumalaysia.php
@@ -20,3 +20,6 @@ echo "<hr>$str = " . date('Y-m-d H:i:s', $str) . ' <= Standard MYSQL date time';
 #--------------------------------------------------------------------------------------------------
 # setelah amin godek
 echo "<hr>$str = " . date('\H\a\r\i \i\n\i j F Y \p\u\k\u\l g:i:s a', $str);
+#--------------------------------------------------------------------------------------------------
+# reload setiap 5 saat https://www.tutorialrepublic.com/faq/how-to-refresh-a-page-periodically-using-php.php
+header("refresh: 2;");
