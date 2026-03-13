e55adf357244290da1a72f1668f711111e1416cc
Amin Di PC HP Elite Tower 800 G9 <al.amin007@yahoo.com>
2026-03-13 11:50:05 +0800

    tukar kod baru

--- sumber/fail/rujukan/csv/dariTeksKeCsv04.php
+++ sumber/fail/rujukan/csv/dariTeksKeCsv04.php
@@ -1,22 +1,64 @@
 <?php
 
 $raw = <<<EOT
-BAHAGIAN 01 : PENGELUARAN TANAMAN DAN TERNAKAN, PEMBURUAN DAN AKTIVITI PERKHIDMATAN BERKAITAN
-Kumpulan 017 : Pemburuan, penjeratan dan aktiviti perkhidmatan berkaitan
+MSIC 2025 SEKSYEN A
+BAHAGIAN 02 : PERHUTANAN DAN PEMBALAKAN
+Kumpulan 021 : Silvikultur dan aktiviti perhutanan lain
 
 KELAS PERKARA KETERANGAN MSIC 2008
-0170(1) Pemburuan, penjeratan dan aktiviti perkhidmatan berkaitan
-01701 Pemburuan dan penjeratan haiwan secara komersial(1) 01701
-01702 Penangkapan haiwan liar (hidup atau mati)(2) 01702
+0210(1) Silvikultur dan aktiviti perhutanan lain
+02101 Penanaman, penanaman semula, pemindahan, pengurusan dan pemuliharaan hutan dan kawasan lapang(2) 02101
+02102 Penanaman untuk tunas semula, kayu pulpa dan kayu api 02102
+02103 Operasi tapak semaian pokok hutan(3) 02103
+02104 Pengumpulan dan pemeliharaan hutan liar(spesis pokok hutan paya gambut) 02104
+02105 Ladang hutan 02105
 
-(1) Termasuk: pengeluaran kulit daripada pemburuan dan penjeratan haiwan <br>Tidak termasuk: <br>(a) pengeluaran kulit berbulu, kulit burung atau reptilia dari operasi  ladang ternakan, lihat 01492 <br>(b) operasi pemeliharaan haiwan untuk sukan di ladang ternakan, lihat 0149 <br>(c) penangkapan ikan paus, lihat 0311 <br>(d) pengeluaran kulit daripada rumah penyembelihan, lihat 1010 <br>(e) pemburuan untuk aktiviti sukan atau rekreasi dan aktiviti perkhidmatan berkaitan, lihat 93199 <br>(f) aktiviti perkhidmatan untuk mempromosi pemburuan dan penjeratan haiwan, lihat 94990
+(1) Tidak termasuk:<br>(a) penanaman pokok krismas, lihat 01299 <br>(b) operasi tapak semaian pokok (bukan hutan), lihat 01304 <br>(c) pengumpulan produk hasil hutan bukan kayu yang tumbuh liar, lihat 0230 <br>(d) pengeluaran reja kayu dan partikel, lihat 16100 <br>(e) aktiviti berkaitan penjagaan pokok hiasan dan arborikultur, lihat 8130
 
-(2) Termasuk: penangkapan haiwan liar (hidup atau mati) untuk makanan, bulu, kulit, untuk kawalan populasi haiwan atau digunakan dalam penyelidikan, di zoo atau haiwan peliharaan
+(2) Termasuk:<br>(a) penanaman semula penambahbaikan (enrichment planting) <br>(b) penanaman pokok bakau <br>(c) penanaman rotan <br>(d) penanaman di kawasan lapang <br>(e) penanaman buluh <br>(f) penanaman di lebuh raya <br>(g) penanaman di persisiran pantai <br>(h) penanaman pokok nibong/pokok sagu
+
+(3) Termasuk: tapak semaian untuk ladang hutan
+
+MSIC 2025 SEKSYEN A
+A - 22
+BAHAGIAN 02 : PERHUTANAN DAN PEMBALAKAN
+Kumpulan 022 : Pembalakan
+
+KELAS PERKARA KETERANGAN MSIC 2008
+0220(11) Pembalakan
+02201 Pengeluaran kayu balak untuk industri berasaskan kayu 02201p
+02202 Pengeluaran kayu balak yang digunakan dalam bentuk belum proses(22) 02202p
+02203 Pengeluaran arang kayu dalam hutan (kaedah tradisional)(33) 02203p
+02204 Pembalakan kayu getah 02204p
+02205 Pengumpulan kayu balak (44) 02201p,02202p,02203p,02204p
+
+(11) Tidak termasuk:<br>(a) penanaman pokok krismas, lihat 01299 <br>(b) penuaian pokok berputar cepat untuk tanaman tenaga (cth. pokok poplar dan willow) secara berasaskan bayaran atau kontrak, lihat 0161 <br>(c) penanaman dirian pokok: penanaman, penanaman semula, pemindahan, penjarangan dan pemuliharaan hutan serta kawasan kayu balak, lihat 02101 <br>(d) pengumpulan produk hasil hutan bukan kayu yang tumbuh liar, lihat 0230 <br>(e) pengeluaran reja kayu dan partikel yang tidak berkaitan dengan pembalakan, lihat 16100 <br>(f) pengeluaran arang melalui distilasi kayu, lihat 16291 
+
+(22) Termasuk: kayu cerucuk, kayu pagar dan kayu jaras
+(33) Termasuk: pengumpulan kulit kayu (spesies hutan darat, hutan paya laut dan gelam) dan kayu api
+(44) Termasuk: pengumpulan dan pengeluaran sisa penuaian hutan oleh isi rumah sahaja, cth. ranting kecil, tunggul, serpihan kayu daripada pembalakan dan sisa hutan lain untuk tenaga
+
+MSIC 2025 SEKSYEN A
+A - 23
+BAHAGIAN 02 : PERHUTANAN DAN PEMBALAKAN
+Kumpulan 023 : Pengumpulan produk hasil hutan bukan kayu
+
+KELAS PERKARA KETERANGAN MSIC 2008
+0230(111) Pengumpulan produk hasil hutan bukan kayu
+02301 Pengumpulan rotan, buluh 02301
+02302 Pemungutan sarang burung 02302
+02303 Pengumpulan pokok sagu liar 02303
+02309 Pengumpulan produk hutan bukan kayu t.t.t.l.(222) 02309
+
+(111) Tidak <br>(a) mengurus pengeluaran bagi mana-mana produk ini (kecuali termasuk: penanaman pokok gabus), lihat bahagian 01 <br>(b) penanaman cendawan atau trufel, lihat 01134 <br>(c) penanaman buah beri atau buah berkulit keras, lihat 0125 <br>(d) pengumpulan kayu api, lihat 02203
+
+(222) Termasuk: <br>(a) pengumpulan sap daripada pokok liar (cth. damar) <br>(b) berserat atau buah-buahan (cth. berangan, keranji, kerdas, petai) <br>(c) daun (cth. bertam, nipah, kajang, kubin, daun kecil) <br>(d) minyak daripada kayu hutan (cth. keruing dll.) <br>(e) akar (cth. akar laka) <br>(f) lilin lebah dan madu <br>(g) cendawan atau trufel <br>(h) buah beri atau kekacang <br>(i) getah balata dan getah seakan getah asli yang lain, gabus, lak dan resin, balsam, biji buah oak, buah berangan kuda, lumut serta liken <br>(j) rambut sayur/rumpai laut (eelgrass) <br>(k) penuaian herba liar dan pengeringan semula jadi yang berkaitan
 
 EOT;
 // -----------------------------------------------
 // Kumpul nota kaki dahulu
 // -----------------------------------------------
+$seksyen = '';
 $footnotes  = [];
 $cleanLines = [];
 $lines      = explode("\n", trim($raw));
@@ -38,17 +80,28 @@ foreach ($lines as $line) {
 $rows = [];
 
 foreach ($cleanLines as $line) {
+	
+	// ----------------------------------------------------------
+	// 1. Kesan "MSIC 2025 SEKSYEN X" — ambil huruf seksyen
+	// ----------------------------------------------------------
+	if (str_starts_with($line, 'MSIC')) {
+		if (preg_match('/^MSIC\s+\d+\s+SEKSYEN\s+([A-Z])/i', $line, $m)) {
+			$seksyen = strtoupper($m[1]);
+		}
+		continue;
+	}
+
 
 	if (str_starts_with($line, 'BAHAGIAN')) {
 		if (preg_match('/BAHAGIAN\s+(\d+)\s*:\s*(.+)/', $line, $m)) {
-			$rows[] = ['', 'A', $m[1], "BAHAGIAN {$m[1]} : " . trim($m[2]), '_', '_'];
+			$rows[] = ['', $seksyen, $m[1], "BAHAGIAN {$m[1]} : " . trim($m[2]), '_', '_'];
 		}
 		continue;
 	}
 
 	if (str_starts_with($line, 'Kumpulan')) {
 		if (preg_match('/Kumpulan\s+(\w+)\s*:\s*(.+)/', $line, $m)) {
-			$rows[] = ['', 'A', $m[1], "Kumpulan {$m[1]} : " . trim($m[2]), '_', '_'];
+			$rows[] = ['', $seksyen, $m[1], "Kumpulan {$m[1]} : " . trim($m[2]), '_', '_'];
 		}
 		continue;
 	}
@@ -80,7 +133,7 @@ foreach ($cleanLines as $line) {
 		}
 
 		$kodPapar = $codeClean . $codeFnSuffix;
-		$rows[] = ['', 'A', $kodPapar, $desc, $msicCode, $noteText];
+		$rows[] = ['', $seksyen, $kodPapar, $desc, $msicCode, $noteText];
 	}
 }
 
