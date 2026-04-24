f1d7fb074f28b44633a603b3e6694adfd87e336c
Amin Di PC HP Elite Tower 800 G9 <al.amin007@yahoo.com>
2026-04-24 09:29:46 +0800

    tambah catatan baru

--- sumber/fail/rujukan/dataEkonomi.php
+++ sumber/fail/rujukan/dataEkonomi.php
@@ -1635,6 +1635,9 @@ $data['bezaUntungRugi'] = array(
 	array('','POM018','Kapasiti pembelian tandan sawit dihadkan bagi mengecilkan atau'
 	. ' memperlahankan operasi','The purchasing capacity of palm bunches is limited to reduce'
 	. ' or slow down operations.'),
+	array('','POM019','gaji kosong sebab pekerja adalah (1) pemilik dan rakan niaga yang aktif'
+	. ' (2) pekerja keluarga tidak bergaji','Empty salaries because workers are 1) working'
+	. ' proprietors and active business partners 2) unpaid family workers'),
 	//array('','zzzzzz','zzzzzz','xxxxxx'),
 );# beza untung rugi
 #--------------------------------------------------------------------------------------------------
