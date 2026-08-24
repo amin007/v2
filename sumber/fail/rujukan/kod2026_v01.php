aa8a9cc38d81582b0987556e9760c58adfef3436
Amin Di PC HP Elite Tower 800 G9 <al.amin007@yahoo.com>
2026-08-24 18:16:21 +0800

    susun semula koding

--- sumber/fail/rujukan/kod2026_v01.php
+++ sumber/fail/rujukan/kod2026_v01.php
@@ -44,7 +44,7 @@ $data['masco2020'] = ImportCSV2Array01($filename = './csv/mascoBM-v06.csv');
 $tajuk['ogcs'] = '#,kod,keterangan ogcs,bahagian';
 $data['ogcs'] = ImportCSV2Array01($filename = './csv/kod ogcs.csv');
 $tajuk['mcpa'] = '#,Version,Mcpa_Class_Code,Mcpa Code,Mcpa_Melayu,Mcpa_En,Cpc,Hs,Sitc,Ahtn,Unit,Catatan';
-$data['mcpa'] = ImportCSV2Array01($filename = './csv/kod mcpa 2009 v1.1.2.csv');
+$data['mcpa'] = ImportCSV2Array01($filename = './csv/kod mcpa 2009 v1.1.4.csv');
 #--------------------------------------------------------------------------------------------------
 $tajuk['msic2008 notakaki'] = '#,s,msic,keterangan,msic2000,notakaki';
 $data['msic2008 notakaki'] = './utama/msic.json';
@@ -58,16 +58,13 @@ $dataJson = ['msic2008 notakaki'];
 ###################################################################################################
 # untuk debug sahaja
 #--------------------------------------------------------------------------------------------------
-$masalahStruktur = ValidasiCsv01($data['mcpa'], 12);
+/*$masalahStruktur = ValidasiCsv01($data['mcpa'], 12);
 $masalah10Digit = ValidasiCSV10Digit01($data['mcpa']);
 if (count($masalahStruktur) > 0)
-{
 	semakPembolehubah($masalahStruktur,'masalah',0);
-}
 if (count($masalah10Digit) > 0)
-{
 	semakPembolehubah($masalah10Digit,'masalah',0);
-}
+//*/
 /*
 $_SERVER['PATH_INFO'] => untuk apache
 $_SERVER['PATH_TRANSLATED'] => untuk apache
