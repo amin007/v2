9a2adeca2f5d3bd18b0f10827fcb69311eef5fcc
Amin Di PC HP Elite Tower 800 G9 <al.amin007@yahoo.com>
2026-09-15 13:11:09 +0800

    tukar kod baru untuk mcpa

--- sumber/fail/rujukan/kod2026.php
+++ sumber/fail/rujukan/kod2026.php
@@ -46,8 +46,9 @@ $tajuk['msic2008 notakaki'] = '#,s,msic,keterangan,msic2000,notakaki';
 $data['msic2008 notakaki'] = './utama/msic.json';
 $tajuk['msic2025 notakaki'] = '#,s,msic,keterangan,msic2008,notakaki';
 $data['msic2025 notakaki'] = './utama/msic2025_notakaki.json';
-$tajuk['mcpa'] = '#,V,Class_Code,Mcpa Code,Mcpa_Melayu,Mcpa_En,Cpc,Hs,Sitc,Ahtn,Unit,Catatan';
-$data['mcpa'] = './kod2026/kod Mcpa 2009v1.1.json';
+$tajuk['mcpa'] = '#,V,Kelas,Kod MCPA,CPC,HS,SITC,AHTN,Unit,Keterangan Melayu,'
+. 'Keterangan Inggeris,Status';
+$data['mcpa'] = './kod2026/kod mcpa Semua.json';
 #--------------------------------------------------------------------------------------------------
 # setkan tatasusunan yang berkaitan dengan fail json
 $dataPhpJson = ['responBE2026','unitKuantitiLampiran16','aup unit kuantiti','bezaUntungRugi'];
